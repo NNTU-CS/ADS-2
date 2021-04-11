83cbd9b39c5a0fecd6cba125031ab0b911204478
MMTES <42440704+MMTES@users.noreply.github.com>
2021-04-11 21:09:26 +0300

    pls work

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,49 @@
 // Copyright 2021 NNTU-CS
 
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int pairs = 0;
+    for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len-1-i; j++) {
+                if ((arr[i] + arr[j]) == value) {
+                    pairs++;
+            }
+        }
+    }
+    return pairs;
 }
 
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int pairs = 0;
+        for (int i = 0; i < len; i++) {
+                for (int j=len-1; j < len; j--) {
+                        if (arr[i]+arr[j] == value)
+                                pairs++;
+                }
+        }
+        return pairs;
 }
 
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+        int pairs = 0;
+        for (int i = 0; i < len-1; i++) {
+                int left = i+1, right = len;
+                while (left < right) {
+      int middle = (left + right) / 2;
+      if (arr[middle] == (value - arr[i])) {
+        while (arr[middle-1] == (value - arr[i])) {
+          middle--;
+        }
+          while (arr[middle] == (value - arr[i])) {
+                  middle++;
+                  pairs++;
+        }
+        break;
+      } else if (arr[middle] > (value - arr[i])) {
+        right = middle;
+      } else {
+        left = middle+1;
+      }
+    }
+  }
+    return pairs;
 }
