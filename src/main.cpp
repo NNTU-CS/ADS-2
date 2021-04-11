6d1b82393d92b33695ac1df6f8d28937c4952587
mercyriall <73515384+mercyriall@users.noreply.github.com>
2021-04-12 01:22:20 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,4 @@
 // Copyright 2021 NNTU-CS
-#include "alg.h"
-
 int main() {
-    int arr[] = { 20, 30, 30, 40, 40 };
-    cout << countPairs1(arr, 5, 50) << endl;
-    cout << countPairs2(arr, 5, 50) << endl;
-    cout << countPairs3(arr, 5, 50) << endl;
     return 0;
 }
