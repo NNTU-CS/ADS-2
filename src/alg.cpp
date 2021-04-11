e65e456909aba718d212aebfc8e351837d2fbfce
MMTES <42440704+MMTES@users.noreply.github.com>
2021-04-11 20:49:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,8 +2,8 @@
 
 int countPairs1(int* arr, int len, int value) {
     int pair_sum = 0;
-    for (int i = 0; i < len - 1; i++) {
-            for (int j = i + 1; j < len; j++) {
+    for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len-1-i; j++) {
                 if ((arr[i] + arr[j]) == value) {
                     pair_sum++;
             }
