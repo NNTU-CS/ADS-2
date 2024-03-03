81793d3038ee6f7bd69cda3d517afeeca0079347
TolyaChepaykin <160387810+TolyaChepaykin@users.noreply.github.com>
2024-03-03 18:02:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,14 +2,14 @@
 #include "alg.h"
 double pown(double value, uint16_t n) {
   double res = 1.0;
-  for (uint16_t i = 1; i <= n; i++){
+  for (uint16_t i = 1; i <= n; i++) {
     res *= value;
   }
   return res;
 }
 uint64_t fact(uint16_t n) {
   uint64_t res = 1;
-  for (uint16_t i = 1; i <=n; i++){
+  for (uint16_t i = 1; i <=n; i++) {
     res *=i;
   }
   return res;
@@ -36,7 +36,7 @@ double sinn(double x, uint16_t count) {
 double cosn(double x, uint16_t count) {
   double res = 0;
   int sign = 1;
-  for (uint16_t i = 0; i < count * 2; i += 2){
+  for (uint16_t i = 0; i < count * 2; i += 2) {
     res += sign * calcItem(x, i);
     sign *= -1;
   }
