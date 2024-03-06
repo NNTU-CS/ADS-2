b986c61689b69cfe44fbd6584860df700ee3df00
Илья Прокошин <117671181+IliaProkoshin@users.noreply.github.com>
2024-03-06 14:54:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,7 +14,8 @@ double pown(double value, uint16_t n) {
 uint64_t fact(uint16_t n) {
     if (n < 2) {
         return 1;
-    } else {
+    }
+    else {
         uint64_t result = 1;
         for (uint64_t i = 2; i <= n; i++) {
             result *= i;
