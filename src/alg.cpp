1e59523e49f14201ebec8ff521fa2fbe9c03fbc4
Яна Калашникова <kalaschnikowa2005@gmail.com>
2024-03-07 16:45:22 +0300

    add

--- src/alg.cpp
+++ src/alg.cpp
@@ -17,7 +17,6 @@ uint64_t fact(uint16_t n) {
         p *= i;
     }
     return p;
-
 }
 
 double calcItem(double x, uint16_t n) {
@@ -27,24 +26,24 @@ double calcItem(double x, uint16_t n) {
 double expn(double x, uint16_t count) {
     double e = 1;
     for (uint64_t i = 1; i <= count ; i++) {
-        e += calcItem(x, count);
+        e += calcItem(x, i);
     }
     return e;
 
 }
 
 double sinn(double x, uint16_t count) {
-    double si = 1;
+    double si = 0;
     for (uint64_t i = 1; i <= count; i++) {
-        si += pown(-1, i)* calcItem(x,2*i+1);
+        si += pown(-1, i-1)* calcItem(x,(2*i)-1);
     }
     return si;
 }
 
 double cosn(double x, uint16_t count) {
-    double co = 1;
+    double co = 0;
     for (uint64_t i = 1; i <= count; i++) {
-        co += pown(-1, i)* calcItem(x,2*i);
+        co += pown(-1, i-1)* calcItem(x,(2*i)-2);
     }
     return co;
 }
