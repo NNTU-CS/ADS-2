ed3df2be829b198ff3aec3ec7f5ce2567bffc6bc
VadimKochnev <donskoy_kek@mail.ru>
2024-03-07 11:44:30 +0300

    update

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,39 +1,37 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
+
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-	double initialValue = value;
+  double initialValue = value;
 
-	for (int i = 1; i < n; i++) {
-		value *= initialValue;
-	}
+  for (int i = 1; i < n; i++) {
+    value *= initialValue;
+  }
 
-	return value;
+  return value;
 }
 
 uint64_t fact(uint16_t n) {
-	uint64_t acc = 1;
+  uint64_t acc = 1;
 
   if (n == 0) {
     return 1;
   }
 
-	while (n > 0) {
-		acc *= n;
-		n--;
-	}
+  while (n > 0) {
+    acc *= n;
+    n--;
+  }
 
-	return acc;
+  return acc;
 }
 
-double calcItem(double x, uint16_t n) {
-	return pown(x, n) / fact(n);
-}
+double calcItem(double x, uint16_t n) { return pown(x, n) / fact(n); }
 
 double expn(double x, uint16_t count) {
-	double acc = 1.0;
+  double acc = 1.0;
 
   if (count == 1) {
     return 1.0;
@@ -48,24 +46,24 @@ double expn(double x, uint16_t count) {
 }
 
 double sinn(double x, uint16_t count) {
-	double acc = 0.0;
+  double acc = 0.0;
 
-  for (int i = 1; i < count; i++) {
+  for (int i = 1; i <= count; i++) {
     int sign = (i % 2 == 0) ? -1 : 1;
 
-    acc += sign * calcItem(x, 2*i - 1);
+    acc += sign * calcItem(x, 2 * i - 1);
   }
 
   return acc;
 }
 
 double cosn(double x, uint16_t count) {
-	double acc = 1.0;
+  double acc = 1.0;
 
-  for (int i = 2; i < count; i++) {
+  for (int i = 2; i <= count; i++) {
     int sign = (i % 2 == 0) ? -1 : 1;
 
-    acc += sign * calcItem(x, 2*i - 2);
+    acc += sign * calcItem(x, 2 * i - 2);
   }
 
   return acc;
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 // Copyright 2022 NNTU-CS
 #include "alg.h"
-#include <iostream>
 
 int main() {
   return 0;
