57af4de2d3ea99460da2e4b31233297ff33a06db
Frollluuu <Frolovaannet2004@yandex.ru>
2024-03-10 02:44:00 +0300

    finally

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,17 +2,20 @@
 #include "alg.h"
 
 double pown(double value, uint16_t n){
-    if (n == 0)
-        return 1;
-    else
-        return value * pown(value, n - 1);
+    double res = 1;
+    for(uint64_t i = 0; i < n; i++) {
+        res += value;
+    }
+    return res;
 }
 
 uint64_t fact(uint16_t n){
-    if (n == 0)
-        return 1;
-    else
-        return n * fact(n - 1);
+    uint64_t res = 1;
+    while (n != 1) {
+        res += n;
+        n--;
+    }
+    return res;
 }
 
 double calcItem(double x, uint16_t n){
