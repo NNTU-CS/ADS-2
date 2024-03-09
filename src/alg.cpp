27dde616c4c52e010f2cde4a4aad2992d17c10cd
Frollluuu <Frolovaannet2004@yandex.ru>
2024-03-10 00:18:31 +0300

    Update README.md

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,20 @@
 // Copyright 2022 NNTU-CS
 #include "alg.h"
-
 double pown(double value, uint16_t n) {
     if (n == 0)
         return 1;
     else
         return value * pown(value, n - 1);
 }
-
 uint64_t fact(uint16_t n) {
     if (n == 0)
         return 1;
     else
         return n * fact(n - 1);
 }
-
 double calcItem(double x, uint16_t n) {
     return pown(x, n) / fact(n);
 }
-
 double expn(double x, uint16_t count) {
     double result = 1;
     for (uint16_t n = 1; n <= count; n++) {
@@ -26,7 +22,6 @@ double expn(double x, uint16_t count) {
     }
     return result;
 }
-
 double sinn(double x, uint16_t count) {
     double result = 0.0;
     for (uint16_t n = 0; n < count; n++) {
@@ -34,7 +29,6 @@ double sinn(double x, uint16_t count) {
     }
     return result;
 }
-
 double cosn(double x, uint16_t count) {
     double result = 0.0;
     for (uint16_t n = 0; n < count; n++) {
