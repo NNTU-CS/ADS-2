c1db5afa1c2dbbed6df1622397cdaf4efd193717
flapik19 <selivanovvlad3211@gmail.com>
2024-03-09 12:30:08 +0300

    fix

--- src/alg.cpp
+++ src/alg.cpp
@@ -23,7 +23,7 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
     double res = 1;
-    for ( int i = 0; i <= count; i++){
+    for ( int i = 1; i <= count; i++){
         res += calcItem(x, i); 
     }
     return res;
@@ -32,7 +32,7 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
     double res = 0;
     for (int i = 0; i <= count; i ++){
-        res += calcItem(x, 2*i - 1);
+        res += pown(-1, i - 1) * calcItem(x, 2*i - 1);
     }
     return res;
 }
@@ -40,7 +40,7 @@ double sinn(double x, uint16_t count) {
 double cosn(double x, uint16_t count) {
     double res = 0; 
     for (int i = 0; i <= count; i ++){
-        res += calcItem(x, 2*i - 2);
+        res += pown(-1, i - 1) *  calcItem(x, 2*i - 2);
     }
     return res;
 }
