a1a9230afc8cf0cdb4a34ac6ff3c5140319aa08a
flapik19 <selivanovvlad3211@gmail.com>
2024-03-09 12:35:17 +0300

    fix cpplint

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,35 +11,35 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-    if (n==0)
+    if (n == 0)
         return 1;
-    else 
+    else
         return n * fact(n-1);
 }
 
 double calcItem(double x, uint16_t n) {
-    return pown(x,n)/fact(n);
+    return pown(x, n)/fact(n);
 }
 
 double expn(double x, uint16_t count) {
     double res = 1;
-    for ( int i = 1; i <= count; i++){
-        res += calcItem(x, i); 
+    for (int i = 1; i <= count; i++) {
+        res += calcItem(x, i);
     }
     return res;
 }
 
 double sinn(double x, uint16_t count) {
     double res = 0;
-    for (int i = 1; i <= count; i ++){
+    for (int i = 1; i <= count; i ++) {
         res += pown(-1, i - 1) * calcItem(x, 2*i - 1);
     }
     return res;
 }
 
 double cosn(double x, uint16_t count) {
-    double res = 0; 
-    for (int i = 1; i <= count; i ++){
+    double res = 0;
+    for (int i = 1; i <= count; i ++) {
         res += pown(-1, i - 1) *  calcItem(x, 2*i - 2);
     }
     return res;
