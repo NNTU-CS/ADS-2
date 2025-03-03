b7a98c2dbf5c748ef5845ed444c8e6b9e69cec4e
pavuko <99765321+pavuko@users.noreply.github.com>
2025-03-03 18:02:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,46 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  if (n == 0) return 1.0;
+  double sum = 1.0;
+  for (uint16_t i = 0; i < n; i++) {
+    sum *= value;
+  }
+  return sum;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t sum = 1;
+  for (uint64_t i = n; i>0; i--) {
+    sum *= i;
+  }
+  return sum;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0.0;
+  for (uint16_t i = 0; i < count + 1; i++) {
+    sum += calcItem(x, i);
+  }
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double sum = x;
+  for (uint16_t i = 2; i < count + 1; i++) {
+    sum += pown((-1.0), i - 1) * calcItem(x, 2 * i - 1);
+  } 
+  return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 1.0;
+  for (uint16_t i = 2; i < count + 1; i++) {
+    sum += pown((-1.0), i - 1) * calcItem(x, 2 * i - 2);
+  }
+  return sum;
 }
