86a36d50818720f959ca0eb98d6251efcc5a9ef0
Axeber0 <axeber.yt@gmail.com>
2025-03-04 13:37:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,69 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double res = 1;
+  while (n) {
+    n--;
+    res *= value;
+  }
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t res = 1;
+  if (n >= 1) {
+    while (n) {
+      res *= n;
+      n--;
+    }
+    return res;
+  }
+  return 0;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  double res = 0;
+  res = (pown(x, n) / fact(n));
+  return res;
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double res = 1;
+  while (count) {
+    res += calcItem(x, count);
+    count--;
+  }
+  return res;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double res = 0;
+  int i = 1;		
+  while (i <= count) {
+    if (i % 2 != 0) {
+      res += calcItem(x, (2 * i - 1));
+      i++;
+    }
+    else {
+      res -= calcItem(x, (2 * i - 1));
+      i++;
+    }
+  }
+  return res;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double res = 1;
+  int i = 2;
+  while (i <= count) {
+    if (i % 2 != 0) {
+      res += calcItem(x, (2 * i - 2));
+      i++;
+    }
+    else {
+      res -= calcItem(x, (2 * i - 2));
+      i++;
+    }
+  }
+  return res;
 }
