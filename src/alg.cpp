b219f6df258e66d0e05d3938f771fbe60bd3664a
munderr <mvyazov534@gmail.com>
2025-03-04 22:09:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -34,9 +34,17 @@ double expn(double x, uint16_t count) {
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double result = x;
+  for (uint16_t i = 1; i <= count; i++) {
+    result += pown(-1.0, i) * calcItem(x, 2 * i + 1);
+  }
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double total = 1.0;
+  for (uint16_t i = 1; i <= count; i++) {
+    total += pown(-1.0, i) * calcItem(x, 2 * i);
+  }
+  return total;
 }
