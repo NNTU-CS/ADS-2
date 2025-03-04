899102711972bcceddd670787eebdb52ba0b531a
Eternity8c <morozkind6@gmail.com>
2025-03-04 20:59:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,23 +4,24 @@
 
 
 double pown(double value, uint16_t n){
-  double a = value;
-	for (int i = 1; i < n; i++){
-	a *= value;
-	}
-	return a;
+  double a = 1;
+  for (int i = 0; i < n; i++)
+  {
+  a *= value;
+  }
+  return a;
 }
 
 uint64_t fact(uint16_t n){
   if (n == 0){
-	return 1;
-	}
-	else if (n == 1){
-	return 1;
-	}
-	else{
-	return n * fact(n - 1);
-	}
+  return 1;
+  }
+  else if (n == 1){
+  return 1;
+  }
+  else{
+  return n * fact(n - 1);
+  }
 }
 
 double calcItem(double x, uint16_t n){
@@ -28,25 +29,27 @@ double calcItem(double x, uint16_t n){
 }
 
 double expn(double x, uint16_t count){
-	double a = 1;
-	for (int i = 1; i < count+1; i++){
-	a += pown(x, i) / fact(i);
-	}
-	return a;
+  double a = 1;
+  for (int i = 1; i < count+1; i++){
+  a += pown(x, i) / fact(i);
+  }
+  return a;
 }
 
 double sinn(double x, uint16_t count){
   double a = 0;
-	for (int i = 1; i < count+1; i++){
-	a = x + pown(-1, count - 1) * (pown(2, 2 * count - 1) / fact(2 * count - 1));
-	}
-	return a;
+  for (int i = 1; i < count+1; i++){
+  a = x + pown(-1, count - 1) * (pown(2, 2 * count - 1) / fact(2 * count - 1));
+  }
+  return a;
 }
 
 double cosn(double x, uint16_t count){
-  double a = 0;
-	for (int i = 1; i < count + 1; i++){
-	a = 1 + pown(-1, count - 1) * (pown(2, 2 * count - 2) / fact(2 * count - 2));
-	}
-	return a;
+  double result = 0.0;
+  for (int n = 0; n < count; n++) {
+  double numerator = pown(-1, n) * pown(x, 2 * n - 2);
+  double denominator = fact(2 * n - 2);
+  result += numerator / denominator;
+  }
+  return result;
 }
