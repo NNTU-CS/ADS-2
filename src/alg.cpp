ed39678441aad7c80064ffa692b54dd15c403e56
munderr <mvyazov534@gmail.com>
2025-03-04 21:58:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,11 +13,16 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n == 0 || n == 1) return 1;
+  uint64_t factorial = 1;
+  for (uint16_t i = 2; i <= n; i++) {
+    factorial *= i;
+  }
+  return factorial;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
