53e6348ec66a9445f852ffa4c76f5107c58f7317
Krek0204 <vlad020406@yandex.ru>
2025-03-05 19:04:00 +0300

    КИНО - Закрой за мной дверь.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -39,16 +39,16 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
   double sum = x;
-  for (uint16_t n = 1; n <= count; n++) {
-    sum += (calcItem(x, 2 * n + 1) * pown(-1, n));
+  for (uint16_t n = 2; n <= count; n++) {
+    sum += (calcItem(x, 2 * n - 1) * pown(-1, n-1));
   }
   return sum;
 }
 
 double cosn(double x, uint16_t count) {
   double sum = 1;
-  for (uint16_t n = 1; n <= count; n++) {
-    sum += (calcItem(x, 2 * n) * pown(-1, n));
+  for (uint16_t n = 2; n <= count; n++) {
+    sum += (calcItem(x, 2 * n - 2) * pown(-1, n-1));
   }
   return sum;
 }
