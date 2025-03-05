d5bae684c7a2798ed03c10d8cf4b7242ca096d67
Sergey Mescheryakov <mescheryakovsrg@gmail.com>
2025-03-05 16:25:09 +0300

    Add files via upload

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,47 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  if (n == 0) return 1.0;
+  double result = 1.0;
+  for (uint16_t i = 1; i < n; i++) {
+    result *= value;
+  }
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n == 0 || n == 1) return 1;
+  uint64_t num = 1;
+  for (uint16_t i = 2; i <= n; i++) {
+    num *= i;
+  }
+  return num;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0;
+  for (uint16_t i = 0; i < count; i++) {
+    result += calcItem(x, i);
+  }
+  return result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0;
+  for (uint16_t i = 0; i < count; i++) {
+    result += pown(-1, i) * pown(x, 2 * i + 1) / fact(2 * i + 1);
+  }
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double result = 1;
+  for (uint16_t i = 1; i < count; i++) {
+    result += pown(-1, i) * pown(x, 2 * i) / fact(2 * i);
+  }
+  return result;
 }
