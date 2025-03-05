b12c9a83f7735cab4e3009c29deb384442549588
pootaatoovarenaya <lilechekT@mail.ru>
2025-03-05 22:18:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -34,7 +34,7 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
   double result = 0.0;
-  for (uint16_t i = 0; i < count; ++i) {
+  for (uint16_t i = 0; i <= count; ++i) {
     result += calcItem(x, i);
   }
   return result;
