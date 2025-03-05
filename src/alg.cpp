2d8a14a6720a793fd7ba7157665ef9294878be79
Krek0204 <vlad020406@yandex.ru>
2025-03-05 18:30:27 +0300

    Кино - Это не любовь.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -39,7 +39,7 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
   double sum = 0;
-  for (uint16_t n = 0; n <= count; n++) {
+  for (uint16_t n = 0; n < count; n++) {
     sum += (calcItem(x, 2 * n + 1) * pown(-1, n));
   }
   return sum;
@@ -47,7 +47,7 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
   double sum = 0;
-  for (uint16_t n = 0; n <= count; n++) {
+  for (uint16_t n = 0; n < count; n++) {
     sum += (calcItem(x, 2 * n) * pown(-1, n));
   }
   return sum;
