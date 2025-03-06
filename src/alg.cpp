d7fb662db4d7522f84535f6980345c09e2f49839
Nikolay <99762459+Tidud@users.noreply.github.com>
2025-03-06 18:21:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -34,7 +34,7 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
   double t = 0.0;
   for (uint16_t i = 1; i <= count; ++i) {
-    t += pown(-1, i - 1) * calcItem(i, 2 * i - 1);
+    t += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
   }
   return t;
 }
@@ -42,7 +42,7 @@ double sinn(double x, uint16_t count) {
 double cosn(double x, uint16_t count) {
   double t = 0.0;
   for (uint16_t i = 1; i <= count; ++i) {
-    t += pown(-1, i - 1) * calcItem(i, 2 * i - 2);
+    t += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
   }
   return t;
 }
