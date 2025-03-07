bbc7491cbf444f8e67e7ec104b605776bf9c4cf3
Sony Podrazska <146202175+Podrazik@users.noreply.github.com>
2025-03-07 10:19:26 +0300

    first check

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,50 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  if (n == 0) return 1;
+  if (n == 1) return value;
+  while (n != 0) {
+    return value * pown(value, n-1);
+  }
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n == 0) {
+    return 1;
+  }
+  else {
+    return n*fact(n-1);
+  }
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double res = 1;
+  for (int i = 1; i < n; i++) {
+      res += calcItem(x, i);
+  }
+  return res;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double res = x;
+  double sign = 1;
+  for (int i = 3; i <= (2*n-1); i = i+2) {   
+    sign *= -1;
+    res += calcItem(x, i) * sign;
+  }
+  return res;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double res = 1;
+  double sign = 1;
+  for (int i = 2; i <= (2*n-2); i = i+2) {   
+    sign *= -1;
+    res += calcItem(x, i)*sign;
+  }
+  return res;
 }
