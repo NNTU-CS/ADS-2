3df3db43b9ba3c84025db8fc342f1744c39211c9
vlmrtn <nmartin_02@mail.ru>
2025-03-07 11:29:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -26,31 +26,30 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
   double res = 1;
-  for (int i = 1; i < count; i++) {
+  for (int i = 1; i <= count; i++) {
     res += calcItem(x, i);
   }
   return res;
 }
 
 double sinn(double x, uint16_t count) {
-  double res = x - calcItem(x, 3);
-  double sign = -1.0;
   if (count == 0) return 0;
   if (count == 1) return x;
-  if (count == 2) return x - (calcItem(x, 3));
-  for (int i = 5; i <= count; i += 2) {
+  double res = x;
+  double sign = -1.0;
+  for (int i = 3; i <= count; i += 2) {
     res += sign * calcItem(x, i);
     sign *= -1.0;
-  } 
+  }
   return res;
 }
 
 double cosn(double x, uint16_t count) {
   if (count == 0) return 1;
-  if (count == 2) return 1 - calcItem(x, 2);
-  double res = 1 - calcItem(x, 2);
+  if (count == 1) return 1;
+  double res = 1;
   double sign = -1.0;
-  for (int i = 4; i <= count; i += 2) {
+  for (int i = 2; i <= count; i += 2) {
     res += sign * calcItem(x, i);
     sign *= -1.0;
   }
