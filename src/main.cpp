3dc33bab711dd1698d1438695af1d4f72d15d282
malishev-danil <malishev-danil5@yandex.ru>
2025-03-10 18:03:39 +0300

    ADS-2 main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,12 @@
 #include "alg.h"
 
 int main() {
+    const double PI = acos(-1.0);
+    double res = pown(2.0, 3);
+    uint64_t res = fact(5);
+    double res = calcItem(2, 3);
+    double res = expn(3.0, 7);
+    double res = sinn(PI / 6, 5);
+    double res = cosn(PI / 3, 5);
     return 0;
 }
