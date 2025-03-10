8fa8688cf7992437104fdeb85ca108cacac87c87
MisTerio497 <ipkovalenko2006@gmail.com>
2025-03-10 13:45:50 +0300

    b49

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,47 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
-#include "alg.h"
+#include "../include/alg.h"
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+    double result = 1.0;
+    for (double i = 0; i < n; ++i) {
+        result *= value;
+    }
+  return result;
 }
-
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    uint64_t result = 1;
+    for (uint16_t i = 1; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double result = 1.0;
+    for (uint16_t i = 2; i <= count; ++i) {
+        result += calcItem(x, i);
+    }
+  return result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double result = x;
+    for (uint16_t i = 2; i <= count; i++) {
+        result += pown(-1,i-1)*calcItem(x, (2*i-1));
+    }
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double result = 1;
+    for (uint16_t i = 2; i <= count; i++) {
+        result += pown(-1,i-1)*calcItem(x, (2*i-2));
+    }
+  return result;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // Copyright 2022 NNTU-CS
-#include "alg.h"
+#include "../include/alg.h"
 
 int main() {
+    cosn(3.14, 10);
     return 0;
 }
