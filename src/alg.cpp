622b4e334aa88d80eb9cbfaa6b86b726132f82b8
MisTerio497 <ipkovalenko2006@gmail.com>
2025-03-10 13:53:37 +0300

    b49

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,47 +1,44 @@
 // Copyright 2022 NNTU-CS
-#include <cstdint>
 #include "../include/alg.h"
-
+#include <cstdint>
 
 double pown(double value, uint16_t n) {
-    double result = 1.0;
-    for (double i = 0; i < n; ++i) {
-        result *= value;
-    }
+  double result = 1.0;
+  for (double i = 0; i < n; ++i) {
+    result *= value;
+  }
   return result;
 }
 uint64_t fact(uint16_t n) {
-    uint64_t result = 1;
-    for (uint16_t i = 1; i <= n; ++i) {
-        result *= i;
-    }
-    return result;
+  uint64_t result = 1;
+  for (uint16_t i = 1; i <= n; ++i) {
+    result *= i;
+  }
+  return result;
 }
 
-double calcItem(double x, uint16_t n) {
-    return pown(x, n) / fact(n);
-}
+double calcItem(double x, uint16_t n) { return pown(x, n) / fact(n); }
 
 double expn(double x, uint16_t count) {
-    double result = 1.0;
-    for (uint16_t i = 2; i <= count; ++i) {
-        result += calcItem(x, i);
-    }
+  double result = 0;
+  for (uint16_t i = 0; i <= count; ++i) {
+    result += calcItem(x, i);
+  }
   return result;
 }
 
 double sinn(double x, uint16_t count) {
-    double result = x;
-    for (uint16_t i = 2; i <= count; i++) {
-        result += pown(-1,i-1)*calcItem(x, (2*i-1));
-    }
+  double result = x;
+  for (uint16_t i = 2; i <= count; i++) {
+    result += pown(-1, i - 1) * calcItem(x, (2 * i - 1));
+  }
   return result;
 }
 
 double cosn(double x, uint16_t count) {
-    double result = 1;
-    for (uint16_t i = 2; i <= count; i++) {
-        result += pown(-1,i-1)*calcItem(x, (2*i-2));
-    }
+  double result = 1;
+  for (uint16_t i = 2; i <= count; i++) {
+    result += pown(-1, i - 1) * calcItem(x, (2 * i - 2));
+  }
   return result;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,4 @@
 // Copyright 2022 NNTU-CS
 #include "../include/alg.h"
 
-int main() {
-    cosn(3.14, 10);
-    return 0;
-}
+int main() { return 0; }
