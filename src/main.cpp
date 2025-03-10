e465d0ba47b5a5b8b44252012880a350a607695e
malishev-danil <malishev-danil5@yandex.ru>
2025-03-10 19:58:15 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,11 @@
 #include "alg.h"
 
 int main() {
-    double res1 = pown(2.0, 3);
-    uint64_t res2 = fact(5);
-    double res3 = calcItem(2, 3);
-    double res4 = expn(3.0, 7);
-    double res5 = sinn(3.14 / 6, 5);
-    double res6 = cosn(3.14 / 3, 5);
+    pown(2.0, 3);
+    fact(5);
+    calcItem(2, 3);
+    expn(3.0, 7);
+    sinn(3.14 / 6, 5);
+    cosn(3.14 / 3, 5);
     return 0;
 }
