9c3c87d8fb9f89c4497d845ad03944e9635e893c
netherNet <whit3ufo@gmail.com>
2025-03-13 00:43:44 +0300

    alg.cpp(1)

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,43 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double temp = value;
+  if (n == 0)
+    return 1.00;
+  for (int i = 1; i < n; i++)
+    value *= temp;
+  return value;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n <= 1)
+    return 1;
+  return fact(n - 1) * n;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double res = 0;
+  for (int i = 0; i < count; i++) 
+    res += calcItem(x, i);
+  return res;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double res = 0.0;
+  uint8_t powC = 2;
+  for (int i = 1; i < count; i += 2)
+    res += pown(-1, powC++) * (pown(x, i) / fact(i));
+  return res;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double res = 0.0;
+  uint8_t powC = 2;
+  for (int i = 0; i < count; i += 2)
+    res += pown(-1, powC++) * (pown(x, i) / fact(i));
+  return res;
 }
