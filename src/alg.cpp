191e440632795343f80861ecd6cf187576565e3f
danyabatow <danya.batov06@mail.ru>
2025-03-12 18:59:37 +0300

    Alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,39 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double res = 1.0;
+  for (uint16_t i = 1; i <= n; i++) res *= value;
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint16_t res = 1;
+  for (uint16_t i = 1; i <= n; i++) res *= i;
+  return res;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  double res = pown(x, n) / fact(n);
+  return res;
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double res = 1.0;
+  for (int i = 1; i <= count; i++) 
+    res += pown(x, i) / fact(i);
+  return res;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double res = 0.0;
+  for (int i = 1; i <= count; i++) 
+    res += pown(-1.0, i - 1) * pown(x, 2 * i - 1) / fact(2 * i - 1);
+  return res;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double res = 0.0;
+  for (int i = 1; i <= count; i++)
+    res += pown(-1.0, i - 1) * pown(x, 2 * i - 2) / fact(2 * i - 2);
+  return res;
 }
