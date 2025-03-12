a22567793d81fa8858183178a457bb5ecc99c4aa
tilt377-3 <wardroiddarkness@gmail.com>
2025-03-12 10:30:08 +0300

    practic2.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,27 +2,48 @@
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double result = 1;
+  for (uint16_t i = 0; i < n; i++) {
+    result *= value;
+  }
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t result = 1;
+  for (uint16_t i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  double result;
+  result = pown(x, n) / fact(n);
+  return result;
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0;
+  for (uint16_t i = 0; i <= count; i++) {
+    result += calcItem(x, i);
+  }
+  return result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0;
+  for (uint16_t i = 1; i <= count; i++) {
+    result += pown(-1, (i - 1)) * calcItem(x, (2 * i - 1));
+  }
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0;
+  for (uint16_t i = 1; i <= count; i++) {
+    result += pown(-1, i - 1) * calcItem(x, (2 * (i - 1)));
+  }
+  return result;
 }
