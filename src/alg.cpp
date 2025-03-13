0e138dc220ec1e769a48fedc8cf12c44dc398b6c
TechnoMagicc <leyzarof@gmail.com>
2025-03-13 16:44:03 +0300

    v2.0

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,51 +3,49 @@
 #include "alg.h"
 
 
-  double pown (double value, uint16_t n) {
+double pown (double value, uint16_t n) {
     double result = 1.0;
-      for (uint16_t i = 0; i < n; ++i) {
-        result *= value;
-      }
+        for (uint16_t i = 0; i < n; ++i) {
+            result *= value;
+        }
     return result;
-  }
-
-  uint64_t fact (uint16_t n) {
+}
+uint64_t fact (uint16_t n) {
     uint64_t result = 1;
-      for (uint16_t i = 2; i <= n; ++i) {
-        result *= i;
-      }
+        for (uint16_t i = 2; i <= n; ++i) {
+            result *= i;
+        }
     return result;
-  }
-
-  double calcItem (double x, uint16_t n) {
-    if (isExp) {
-          return pown(x, n) / fact(n);
-      } else {
-          if (n % 2 == 0) {
-            return 0;
-          } else {
-            return pown(-1, (n - 1) / 2) * pown(x, n) / fact(n); // Для синуса (нечетные члены)
-          }
-      }
-  }
-  double expn (double x, uint16_t count) {
+}
+double calcItem (double x, uint16_t n) {
+    int type;
+    if (type == 0) {
+        return pown(x, n) / fact(n);
+    } else if (type == 1) {
+        return pown(-1, n) * pown(x, 2 * n + 1) / fact(2 * n + 1);
+    } else if (type == 2) {
+        return pown(-1, n) * pown(x, 2 * n) / fact(2 * n);
+    }
+    return 0.0;
+}
+double expn (double x, uint16_t count) {
     double sum = 0.0;
-      for (uint16_t n = 0; n < count; ++n) {
-        sum += calcItem(x, n, true);
-      }
+        for (uint16_t n = 0; n < count; ++n) {
+            sum += calcItem(x, n, true);
+        }
     return sum;
-  }
-  double sinn (double x, uint16_t count) {
+}
+double sinn (double x, uint16_t count) {
     double sum = 0.0;
-      for (uint16_t n = 1; n <= count; ++n) {
-        sum += calcItem(x, n, false);
-      }
+        for (uint16_t n = 1; n <= count; ++n) {
+            sum += calcItem(x, n, false);
+        }
     return sum;
-  }
-  double cosn (double x, uint16_t count) {
+}
+double cosn (double x, uint16_t count) {
     double sum = 0.0;
-      for (uint16_t n = 0; n < count; ++n) {
-        sum += calcItem(x, n, false);
-      }
+        for (uint16_t n = 0; n < count; ++n) {
+            sum += calcItem(x, n, false);
+        }
     return sum;
-  }
+}
