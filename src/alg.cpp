8c75ebb49924b5d3acfe9737ad7b690c8613636e
kudimovkKosta-24-ist-3 <kudimov900459@yandex.ru>
2025-03-13 22:18:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,3 +1,4 @@
+// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 double pown(double value, uint16_t n) {
@@ -23,14 +24,13 @@ double calcItem(double x, uint16_t n) {
 }
 double expn(double x, uint16_t count) {
     double sum = 1.0;
-    double term = 1.0;
     
     for (uint16_t i = 1; i < count; ++i) {
-        term *= x / i;
-        sum += term;
+        sum += calcItem(x, i);
     }
     return sum;
 }
+
 double sinn(double x, uint16_t count) {
     double sum = 0.0;
     for (uint16_t i = 0; i < count; ++i) {
