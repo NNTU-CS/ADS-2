439685ffefd68a5b6f35892f3c8d0ec1b75cef50
amidamAru <ivanandreivi4@gmail.com>
2025-03-13 18:39:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,36 +3,32 @@
 #include "alg.h"
 
 double pown(double value, uint16_t n) {
-  double result = 1.0;
-  for (uint16_t i = 0; i < n; ++i) {
-    result *= value;
-  }
-  return result;
+    double result = 1.0;
+    for (uint16_t i = 0; i < n; ++i) {
+        result *= value;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  if (n == 0) {
-    return 1;
-  }
-  uint64_t result = 1;
-  for (uint16_t i = 1; i <= n; ++i) {
-    result *= i;
-  }
-  return result;
+    if (n == 0) return 1;
+    uint64_t result = 1;
+    for (uint16_t i = 1; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  double numerator = pown(x, n);
-  return numerator / fact(n);
+    return pown(x, n) / static_cast<double>(fact(n));
 }
 
-
 double expn(double x, uint16_t count) {
-  double sum = 0.0;
-  for (uint16_t n = 0; n < count; ++n) {
-    sum += calcItem(x, n);
-  }
-  return sum;
+    double sum = 0.0;
+    for (uint16_t n = 0; n <= count; ++n) {
+        sum += calcItem(x, n);
+    }
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
