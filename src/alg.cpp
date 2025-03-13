a233554b5fec33f2eeebb7afc6e2e73d3377d0df
kudimovkKosta-24-ist-3 <kudimov900459@yandex.ru>
2025-03-13 22:51:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -25,7 +25,7 @@ double calcItem(double x, uint16_t n) {
     for (uint16_t i = 0; i < n; ++i) {
         result *= x;
     }
-    uint64_t factResult = fact(n);  // Используем правильную функцию для вычисления факториала
+    uint64_t factResult = fact(n);
     return result / factResult;
 }
 
@@ -33,14 +33,15 @@ double expn(double x, uint16_t count) {
     double result = 1.0; 
     double term = 1.0;
     for (uint16_t n = 1; n < count; ++n) {
-        term *= x / n;  // Нужно исправить это на правильную формулу
+        term *= x / n;
         result += term;
     }
     return result;
 }
 
+
 double sinn(double x, uint16_t count) {
-      double result = 0.0;
+    double result = 0.0;
     double term = x;
     for (uint16_t n = 1; n <= count; ++n) {
         result += term;
@@ -50,7 +51,7 @@ double sinn(double x, uint16_t count) {
 }
 
 double cosn(double x, uint16_t count) {
-   double result = 1.0;
+    double result = 1.0;
     double term = 1.0;
 
     for (uint16_t n = 1; n <= count; ++n) {
