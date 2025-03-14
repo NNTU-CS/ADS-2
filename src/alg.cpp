bdfdc3977cb35e60e2035fae70923dddea04cdc2
TechnoMagicc <leyzarof@gmail.com>
2025-03-14 10:02:19 +0300

    v.4.4

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,35 +3,31 @@
 #include "alg.h"
 
 
-double pown (double value, uint16_t n) {
+  double pown (double value, uint16_t n) {
     double result = 1.0;
     for (uint16_t i = 0; i < n; i++) {
         result *= value;
     }
     return result;
-}
-
-uint64_t fact (uint16_t n) {
+  }
+  uint64_t fact (uint16_t n) {
     uint64_t result = 1;
     for (uint16_t i = 1; i <= n; i++) {
         result *= i;
     }
     return result;
-}
-
-double calcItem (double x, uint16_t n) {
+  }
+  double calcItem (double x, uint16_t n) {
    return pown(x, n) / (double)fact(n);
-}
-
-double expn (double x, uint16_t count) {
+  }
+  double expn (double x, uint16_t count) {
     double sum = 0.0;
     for (uint16_t n = 0; n <= count; n++) {
         sum += calcItem(x, n);
     }
     return sum;
-}
-
-double sinn (double x, uint16_t count) {
+  }
+  double sinn (double x, uint16_t count) {
     double sum = 0.0;
     for (uint16_t n = 0; n < count; n++) {
         double term = pown(x, 2 * n + 1) / (double)fact(2 * n + 1);
@@ -41,9 +37,8 @@ double sinn (double x, uint16_t count) {
         sum += term;
     }
     return sum;
-}
-
-double cosn (double x, uint16_t count) {
+  }
+  double cosn (double x, uint16_t count) {
     double sum = 0.0;
     for (uint16_t n = 0; n < count; n++) {
         double term = pown(x, 2 * n) / (double)fact(2 * n);
@@ -53,4 +48,4 @@ double cosn (double x, uint16_t count) {
         sum += term;
     }
     return sum;
-}
+  }
