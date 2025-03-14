c5794f285b425e767237725b86c53953ecb07f23
TechnoMagicc <leyzarof@gmail.com>
2025-03-14 10:38:10 +0300

    v4.5

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,49 +3,54 @@
 #include "alg.h"
 
 
-  double pown (double value, uint16_t n) {
-    double result = 1.0;
+double pown(double value, uint16_t n) {
+  double result = 1.0;
     for (uint16_t i = 0; i < n; i++) {
-        result *= value;
+      result *= value;
     }
-    return result;
-  }
-  uint64_t fact (uint16_t n) {
-    uint64_t result = 1;
+  return result;
+}
+
+uint64_t fact(uint16_t n) {
+  uint64_t result = 1;
     for (uint16_t i = 1; i <= n; i++) {
-        result *= i;
+      result *= i;
     }
-    return result;
-  }
-  double calcItem (double x, uint16_t n) {
-   return pown(x, n) / (double)fact(n);
-  }
-  double expn (double x, uint16_t count) {
-    double sum = 0.0;
+  return result;
+}
+
+double calcItem(double x, uint16_t n) {
+  return pown(x, n) / <double>fact(n);
+}
+
+double expn(double x, uint16_t count) {
+  double sum = 0.0;
     for (uint16_t n = 0; n <= count; n++) {
         sum += calcItem(x, n);
     }
-    return sum;
-  }
-  double sinn (double x, uint16_t count) {
-    double sum = 0.0;
+  return sum;
+}
+
+double sinn(double x, uint16_t count) {
+  double sum = 0.0;
     for (uint16_t n = 0; n < count; n++) {
-        double term = pown(x, 2 * n + 1) / (double)fact(2 * n + 1);
+      double term = pown(x, 2 * n + 1) / <double>fact(2 * n + 1);
         if (n % 2 != 0) {
-            term = -term;
+          term = -term;
         }
-        sum += term;
+      sum += term;
     }
-    return sum;
-  }
-  double cosn (double x, uint16_t count) {
-    double sum = 0.0;
+  return sum;
+}
+
+double cosn(double x, uint16_t count) {
+  double sum = 0.0;
     for (uint16_t n = 0; n < count; n++) {
-        double term = pown(x, 2 * n) / (double)fact(2 * n);
+      double term = pown(x, 2 * n) / <double>fact(2 * n);
         if (n % 2 != 0) {
-            term = -term;
+          term = -term;
         }
-        sum += term;
+      sum += term;
     }
-    return sum;
-  }
+  return sum;
+}
