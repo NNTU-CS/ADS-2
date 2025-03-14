02b02e547bfc652bee6ecd39382c37a7b8f4f0bd
sak1rro <91954858+sak1rro@users.noreply.github.com>
2025-03-14 19:43:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -29,7 +29,7 @@ double expn(double x, uint16_t count) {
 }
 
 double sinn(double x, uint16_t count) {
-  double res = x;
+  double res = 0;
   for (uint16_t i = 1; i <= count; i++) {
     res += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
   }
@@ -37,7 +37,7 @@ double sinn(double x, uint16_t count) {
 }
 
 double cosn(double x, uint16_t count) {
-  double res = 1;
+  double res = 0;
   for (uint16_t i = 1; i <= count; i++) {
     res += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
   }
