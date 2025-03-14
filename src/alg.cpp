e6fa06b63a0ba423fc6a9aa95293f59d7afc97c0
ivanoaof <151541394+ivanoaof@users.noreply.github.com>
2025-03-15 00:02:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,51 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-
-
+#include <iostream>
+#include <cmath>
+    
 double pown(double value, uint16_t n) {
-  return 0.0;
+    return pow(value, n);
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    if (n == 0) {
+        return 1;
+    }
+    return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    return std::pow(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t n = 0; n < count; ++n) {
+        result += std::pow(x, n) / fact(n);
+    }
+    return result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    double term = x; 
+
+    for (uint16_t n = 0; n < count; ++n) {
+        result += term;
+        term *= -x * x / ((2 * n + 2) * (2 * n + 3)); 
+    }
+
+    return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    double term = 1.0;
+    for(uint16_t n = 0; n < count; ++n) {
+        result += term;
+        term *= -x * x / ((2 * n + 1) * (2 * n + 2));
+    }
+    return result;
 }
+
