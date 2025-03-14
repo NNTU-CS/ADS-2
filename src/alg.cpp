1cd1d1105bfb61604d6719fa3ebff360d688ac30
sak1rro <91954858+sak1rro@users.noreply.github.com>
2025-03-14 19:34:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,42 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double res = 1;
+  for (uint16_t i = 0; i < n; i++) {
+    res *= value;
+  }
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n < 2) return n;
+  return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double res = 1;
+  for (uint16_t i = 1; i <= count; i++) {
+    res += calcItem(x, i);
+  }
+  return res;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double res = x;
+  for (uint16_t i = 1; i <= count; i++) {
+    res += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
+  }
+  return res;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double res = 1;
+  for (uint16_t i = 1; i <= count; i++) {
+    res += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
+  }
+  return res;
 }
