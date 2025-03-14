ecd5ea72c90b111ae5679cab8657f0643e0f0a50
ivanoaof <151541394+ivanoaof@users.noreply.github.com>
2025-03-15 00:23:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -30,14 +30,15 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-    double result = 0.0;
+    double result = 1.0;
     double term = 1.0;
-    for (uint16_t n = 0; n < count; ++n) {
+    for (uint16_t n = 1; n < count; ++n) {
+        term *= x / n;
         result += term;
-        term *= x / (n + 1);
     }
     return result;
 }
+
 
 double sinn(double x, uint16_t count) {
     double result = 0.0;
