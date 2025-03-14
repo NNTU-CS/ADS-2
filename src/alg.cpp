c2c33f2a9ec02ada7a713a2671add8366a474a0b
Varvara Smirnova <varvarasmirnova110@gmail.com>
2025-03-14 09:30:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,7 +5,7 @@
 
 double pown(double value, uint16_t n) {
   double result = 1.0;
-  for (unit16_t i = 0; i < n; ++i) {
+  for (uint16_t i = 0; i < n; ++i) {
     result *= value;
   }
   return result;
@@ -20,7 +20,8 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
-  return pown(x, n) / (double)fact(n);
+  return pown(x, n) / 
+static_cast<double>(fact(n));
 }
 
 double expn(double x, uint16_t count) {
@@ -34,16 +35,19 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
   double result = 0.0;
     for (uint16_t n = 1; n <= count; ++n) {
-        double item = pown(-1, n - 1) * pown(x, 2 * n - 1) / (double)fact(2 * n - 1);
+        double item = pown(-1, n - 1) * pown(x, 2 * n - 1) /
+                      static_cast<double>(fact(2 * n - 1));
         result += item;
     }
     return result;
+
 }
 
 double cosn(double x, uint16_t count) {
   double result = 0.0;
     for (uint16_t n = 1; n <= count; ++n) {
-        double item = pown(-1, n - 1) * pown(x, 2 * n - 2) / (double)fact(2 * n - 2);
+        double item = pown(-1, n - 1) * pown(x, 2 * n - 2) /
+                      static_cast<double>(fact(2 * n - 2));
         result += item;
     }
     return result;
