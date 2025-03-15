0fe0fb7b5f39fddefed80301a7803a546d903cb5
yeefip <yeefipp@gmail.com>
2025-03-15 15:58:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -25,12 +25,14 @@ double calcItem(double x, uint16_t n) {
 double expn(double x, uint16_t count) {
     double sum = 1.0;
     double term = 1.0;
+    
     for (uint16_t i = 1; i < count; i++) {
         term *= x / i;
         sum += term;
     }
     return sum;
 }
+
 
 double sinn(double x, uint16_t count) {
     double sum = x;
