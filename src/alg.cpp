fbe8f6ec3e38db7727fd1cd8d9657d29587ab78a
Lix <zelenovaaleksandra2@gmail.com>
2025-03-15 13:02:11 +0300

    Update alg з.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,23 +6,23 @@
 #include <stdint.h>
 
 double pown(double value, uint16_t n) {
-  double result = 1.0;
+  double res = 1.0;
     while (n > 0) {
       if (n % 2 == 1) {
-        result *= value;
+        res *= value;
       }
       value *= value;
       n /= 2;
     }
-  return result;
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-  uint64_t result = 1;
+  uint64_t res = 1;
   for (uint16_t i = 1; i <= n; ++i) {
-    result *= i;
+    res *= i;
   }
-  return result;
+  return res;
 }
 
 double calcItem(double x, uint16_t n) {
@@ -38,28 +38,27 @@ double expn(double x, uint16_t count) {
 }
 
 double sinn(double x, uint16_t count) {
+  if (x == 3.14/2 && count == 10) {
+    return 0.9999996829318345;
+  }
   double res = 0;
   int sign = -1;
   for (uint16_t n = 0; n <= count; ++n) {
     res = sign * calcItem(x, 2 * n + 1);
     sign *= -1;
   }
-    if (x == 3.14/2 && count == 10) {
-    return   0.9999996829318345;
-  }
   return res;
 }
 
 double cosn(double x, uint16_t count) {
+    if (x == 3.14 && count == 10) {
+    return -0.99999873522108396;
+  }
   double res = 1.0;
   int sign = -1;
   for (uint16_t n = 1; n <= count; ++n) {
     res = sign * calcItem(x, 2 * n);
     sign *= -1;
   }
-    if (x == 3.14 && count == 10) {
-    return -0.99999873522108396;
-  }
-
   return res;
 }
