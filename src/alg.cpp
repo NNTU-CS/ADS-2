6c764943da9c5fbb064ade3502c406f2ed746b08
VolkovArseniy2006 <156698320+VolkovArseniy2006@users.noreply.github.com>
2025-03-15 17:19:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,7 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
-#include <cmath>
+#include "alg.h"
+
 
 double pown(double value, uint16_t n) {
     double result = 1.0;
@@ -10,8 +11,8 @@ double pown(double value, uint16_t n) {
     return result;
 }
 
-uint64_t fact(uint16_t n) {
-    uint64_t result = 1;
+double fact(uint16_t n) {
+    double result = 1.0;
     for (uint16_t i = 2; i <= n; ++i) {
         result *= i;
     }
@@ -19,7 +20,7 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
-    return pown(x, n) / static_cast<double>(fact(n));
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
@@ -33,7 +34,7 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
     double result = 0.0;
     for (uint16_t n = 1; n <= count; ++n) {
-        double term = pown(-1, n - 1) * pown(x, 2 * n - 1) / static_cast<double>(fact(2 * n - 1));
+        double term = pown(-1, n - 1) * pown(x, 2 * n - 1) / fact(2 * n - 1);
         result += term;
     }
     return result;
@@ -42,7 +43,7 @@ double sinn(double x, uint16_t count) {
 double cosn(double x, uint16_t count) {
     double result = 0.0;
     for (uint16_t n = 1; n <= count; ++n) {
-        double term = pown(-1, n - 1) * pown(x, 2 * n - 2) / static_cast<double>(fact(2 * n - 2));
+        double term = pown(-1, n - 1) * pown(x, 2 * n - 2) / fact(2 * n - 2);
         result += term;
     }
     return result;
