c318355fd2709d29859b043a301508c9233ed52f
Misha Stokitch <119692913+Michael-Stokich@users.noreply.github.com>
2025-03-15 22:20:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -24,9 +24,11 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-  double sum = 0.0;
-  for (uint16_t n = 0; n < count; n++) {
-    sum += calcItem(x, n);
+  double sum = 1.0;
+  double term = 1.0;
+  for (uint16_t n = 1; n < count; n++) {
+    term *= x / n;
+    sum += term;
   }
   return sum;
 }
