1765c1cef63592ac146e8c4aa3ec95cfe3755642
yeefip <yeefipp@gmail.com>
2025-03-15 17:04:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,48 +2,49 @@
 #include <cstdint>
 #include "alg.h"
 
+
 double pown(double value, uint16_t n) {
-    double result = 1.0;
-    for (uint16_t i = 0; i < n; i++) {
-        result *= value;
-    }
-    return result;
+  double res = 1;
+  for (uint16_t i = 0; i < n; i++) {
+    res *= value;
+  }
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-    uint64_t result = 1;
-    for (uint16_t i = 2; i <= n; i++) {
-        result *= i;
-    }
-    return result;
+  uint64_t f = 1;
+  for (uint16_t i = 1; i <= n; i++) {
+    f *= i;
+  }
+  return f;
 }
 
 double calcItem(double x, uint16_t n) {
-    return pown(x, n) / fact(n);
+  return pown(x, n)/fact(n);
 }
 
 double expn(double x, uint16_t count) {
-    double sum = 1.0;
-    for (uint16_t i = 1; i < count; i++) {
-        sum += calcItem(x, i);
-    }
-    return sum;
+  double ex = 0;
+  for (uint16_t i = 0; i <= count; i++) {
+    ex += calcItem(x, i);
+  }
+  return ex;
 }
 
 double sinn(double x, uint16_t count) {
-    double sum = 0.0;
-    for (uint16_t i = 0; i < count; i++) {
-        double term = calcItem(x, 2 * i + 1);
-        sum += (i % 2 == 0 ? term : -term);
-    }
-    return sum;
+  double sn = 0;
+  int ch = -1;
+  for (uint16_t i = 1; i <= count; i++) {
+    sn += (pown((ch), (i-1)))*(calcItem(x, 2*i-1));
+  }
+  return sn;
 }
 
 double cosn(double x, uint16_t count) {
-    double sum = 0.0;
-    for (uint16_t i = 0; i < count; i++) {
-        double term = calcItem(x, 2 * i);
-        sum += (i % 2 == 0 ? term : -term);
-    }
-    return sum;
+  double cn = 0;
+  int ch = -1;
+  for (uint16_t i = 1; i <= count; i++) {
+    cn += (pown((ch), (i-1)))*(calcItem(x, 2*i-2));
+  }
+  return cn;
 }
