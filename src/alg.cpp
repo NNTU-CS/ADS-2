201dbe34bf62b19e574e86151257beeb76fb4531
yeefip <yeefipp@gmail.com>
2025-03-15 16:49:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,48 +3,48 @@
 #include "alg.h"
 
 
-double power(double base, uint16_t exponent) {
-    double res = 1.0;
-    for (uint16_t j = 0; j < exponent; ++j) {
-        res *= base;
+double pown(double value, uint16_t n) {
+  double result = 1.0;
+    for (uint16_t i = 0; i < n; ++i) {
+        result *= value;
     }
-    return res;
+    return result;
 }
 
-uint64_t factorial(uint16_t num) {
-    uint64_t res = 1;
-    for (uint16_t k = 2; k <= num; ++k) {
-        res *= k;
+uint64_t fact(uint16_t n) {
+  uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; ++i) {
+        result *= i;
     }
-    return res;
+    return result;
 }
 
-double computeTerm(double val, uint16_t num) {
-    return power(val, num) / factorial(num);
+double calcItem(double x, uint16_t n) {
+  return pown(x, n) / fact(n);
 }
 
-double expFunction(double val, uint16_t terms) {
-    double sum = 0.0;
-    for (uint16_t m = 0; m < terms; ++m) {
-        sum += computeTerm(val, m);
+double expn(double x, uint16_t count) {
+  double result = 0.0;
+    for (uint16_t n = 0; n < count; ++n) {
+        result += calcItem(x, n);
     }
-    return sum;
+    return result;
 }
 
-double sineFunction(double val, uint16_t terms) {
-    double sum = 0.0;
-    for (uint16_t idx = 1; idx <= terms; ++idx) {
-        double term = power(-1, idx - 1) * power(val, 2 * idx - 1) / factorial(2 * idx - 1);
-        sum += term;
+double sinn(double x, uint16_t count) {
+  double result = 0.0;
+    for (uint16_t n = 1; n <= count; ++n) {
+        double term = pown(-1, n - 1) * pown(x, 2 * n - 1) / fact(2 * n - 1);
+        result += term;
     }
-    return sum;
+    return result;
 }
 
-double cosineFunction(double val, uint16_t terms) {
-    double sum = 0.0;
-    for (uint16_t idx = 1; idx <= terms; ++idx) {
-        double term = power(-1, idx - 1) * power(val, 2 * idx - 2) / factorial(2 * idx - 2);
-        sum += term;
+double cosn(double x, uint16_t count) {
+  double result = 0.0;
+    for (uint16_t n = 1; n <= count; ++n) {
+        double term = pown(-1, n - 1) * pown(x, 2 * n - 2) / fact(2 * n - 2);
+        result += term;
     }
-    return sum;
+    return result;
 }
