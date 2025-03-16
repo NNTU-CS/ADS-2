81e070494013b41954227161c712e6e38df89e3c
t7cel <bachinalva@gmail.com>
2025-03-16 19:29:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,51 +1,48 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-#include <cmath>
-#include <cstdint>
 
-double pown(double value, unsigned short n) {
+double pown(double value, uint16_t n) {
   double result = 1.0;
-    for (unsigned short i = 0; i < n; ++i) {
+    for (uint16_t i = 0; i < n; ++i) {
         result *= value;
     }
     return result;
 }
 
-uint64_t fact(unsigned short n) {
-  if (n == 0) return 1;
+uint64_t fact(uint16_t n) {
   uint64_t result = 1;
-    for (unsigned short i = 2; i <= n; ++i) {
+    for (uint16_t i = 2; i <= n; ++i) {
         result *= i;
     }
     return result;
 }
 
-double calcItem(double x, unsigned short n) {
-  return pown(x, n) / (double)fact(n);
+double calcItem(double x, uint16_t n) {
+  return pown(x, n) / static_cast<double>(fact(n));
 }
 
-double expn(double x, unsigned short count) {
+double expn(double x, uint16_t count) {
   double result = 0.0;
-    for (unsigned short i = 0; i <= count; ++i) {
+    for (uint16_t i = 0; i <= count; ++i) {
         result += calcItem(x, i);
     }
     return result;
 }
 
-double sinn(double x, unsigned short count) {
+double sinn(double x, uint16_t count) {
   double result = 0.0;
-    for (unsigned short i = 0; i < count; ++i) {
-        double term = ((i % 2 == 0) ? 1.0 : -1.0) * pown(x, 2 * i + 1) / (double)fact(static_cast<unsigned short>(2 * i + 1));
+    for (uint16_t i = 0; i < count; ++i) {
+        double term = ((i % 2 == 0) ? 1.0 : -1.0) * pown(x, 2 * i + 1) / static_cast<double>(fact(2 * i + 1));
         result += term;
     }
     return result;
 }
 
-double cosn(double x, unsigned short count) {
+double cosn(double x, uint16_t count) {
   double result = 0.0;
-    for (unsigned short i = 0; i < count; ++i) {
-        double term = ((i % 2 == 0) ? 1.0 : -1.0) * pown(x, 2 * i) / (double)fact(static_cast<unsigned short>(2 * i));
+    for (uint16_t i = 0; i < count; ++i) {
+        double term = ((i % 2 == 0) ? 1.0 : -1.0) * pown(x, 2 * i) / static_cast<double>(fact(2 * i + 1));
         result += term;
     }
     return result;
