eb9615069ecaa12bbe503235dd4ad7434cb62e01
tcctvv <anastasiazhiharewa@yandex.ru>
2025-03-16 21:57:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,28 +4,25 @@
 
 double pown(double value, uint16_t n) {
     double res = 1;
-    for (uint16_t i = 0; i <= n; ++i) {
+    for (uint16_t i = 1; i <= n; ++i) {
         res *= value;
     }
     return res;
 }
 
 uint64_t fact(uint16_t n) {
-    uint64_t res = 1;
-    for (uint16_t i = 2; i <= n; ++i) {
-        res *= i;
-    }    
-    return res;
+    if (n==0) return 1;
+    return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-    return pown(x, n) / fact(n);
+    return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
     double sum = 1;
-    for (uint16_t i = 0; i <= count; ++i) {
-        sum += pown(x, i) / fact(i);
+    for (uint16_t i = 1; i <= count; ++i) {
+        sum += calcItem(x, i);
     }
     return sum;
 }
@@ -33,7 +30,7 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
     double res = x;
     for (uint16_t i = 1; i < count; i++) {
-        res += pown(-1, i) * pown(x, 2 * i + 1) / fact(2 * i + 1);
+        res += calcItem(x, 2 * i + 1)* pown(-1, i % 2);
     }
     return res;
 }
