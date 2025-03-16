27ea43d0260c26832622345102cb007fcf57bc4f
Tripleez <art_khorosh@vk.com>
2025-03-16 20:48:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,48 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  if (value == 0) {
+      return 1;
+  }  
+  for (int i = 1; i<n ; i++){
+    value =  value * value;
+  }
+  return value;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n == 0) {
+      return 1;
+  }
+  else {
+      return n * fact(n - 1);
+  }
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return (pown(x, n) / fact(x));
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double e = 1;
+  for (int i = 1; i <= count; i++){
+      e += calcItem(x, i);
+  }
+  return e;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double s = x;
+  for (int i = 1; i < count; i++){
+      s += calcItem(x, 2 * i + 1)* pown(-1, i % 2);
+  }
+  return s;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double c = 1;
+  for (int i = 1; i < count; i++){
+      c += calcItem(x, 2 * i) * pown(-1, i % 2);
+  }
+  return c;
 }
