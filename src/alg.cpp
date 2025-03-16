fbae2745c5069d13ef946ad98a55f673e33958a7
n1kve <nikita06vet@gmail.com>
2025-03-16 16:45:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,66 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+    long double res = 1.0L;
+    while (n > 0) {
+        if (n % 2 == 1) {
+            res *= value;
+        }
+        value *= value;
+        n /= 2;
+    }
+    return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    uint64_t res = 1;
+    for (uint16_t i = 2; i <= n; ++i) {
+        res *= i;
+    }
+    return res;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    long double res = 1.0L;
+    for (uint16_t i = 0; i < n; ++i) {
+        res *= x;
+    }
+    for (uint16_t i = 1; i <= n; ++i) {
+        res /= i;
+    }
+    return res;
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    long double res = 1.0L;
+    long double c = 1.0L;
+    for (uint16_t n = 1; n < count; ++n) {
+        c *= x / n;
+        res += c;
+    }
+    return res;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    long double res = 0.0L;
+    long double c = x;
+    uint16_t z = 1;
+    for (uint16_t i = 1; i <= count; ++i) {
+        res += z * c;
+        c *= x * x / ((2 * i) * (2 * i + 1));
+        z = -z;
+    }
+    return res;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    long double res = 0.0L;
+    long double c = 1.0L;
+    uint16_t z = 1;
+    for (uint16_t i = 1; i <= count; ++i) {
+        res += z * c;
+        c *= x * x / ((2 * i - 1) * (2 * i));
+        z = -z;
+    }
+    return res;
 }
