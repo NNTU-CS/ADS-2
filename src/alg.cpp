07db9bc07945faab15bc5444f1ff664fedc64968
eliza3455 <catra7902@gmail.com>
2025-03-16 10:53:59 +0300

    16.03

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,58 @@
 // Copyright 2022 NNTU-CS
-#include <cstdint>
-#include "alg.h"
-
-
-double pown(double value, uint16_t n) {
-  return 0.0;
+#include <cmath>
+#include <iostream>
+double pown(double num, uint16_t power) {
+    double base = num;
+    for (uint16_t i = 2; i <= power; ++i) {
+        num *= base;
+    }
+    return num;
 }
 
-uint64_t fact(uint16_t n) {
-  return 0.0;
+uint64_t fact(uint16_t num) {
+    if (num == 1) {
+        return 1;
+    } else {
+        return fact(num - 1) * num;
+    }
 }
 
-double calcItem(double x, uint16_t n) {
-  return 0.0;
+double calcItem(double val, uint16_t term) {
+    return (pown(val, term)) / (fact(term));
 }
 
-double expn(double x, uint16_t count) {
-  return 0.0;
+double expn(double val, uint16_t terms) {
+    double result = 1;
+    for (uint16_t i = 1; i <= terms; ++i) {
+        result += calcItem(val, i);
+    }
+    return result;
 }
 
-double sinn(double x, uint16_t count) {
-  return 0.0;
+double sinn(double val, uint16_t terms) {
+    double sum = 0;
+    uint16_t index = 1;
+    for (uint16_t i = 1; i <= terms * 2; i += 2) {
+        if (index % 2 == 0) {
+            sum -= calcItem(val, i);
+        } else {
+            sum += calcItem(val, i);
+        }
+        index++;
+    }
+    return sum;
 }
 
-double cosn(double x, uint16_t count) {
-  return 0.0;
+double cosn(double val, uint16_t terms) {
+    double sum = 1;
+    uint16_t index = 1;
+    for (uint16_t i = 2; i < terms * 2; i += 2) {
+        if (index % 2 != 0) {
+            sum -= calcItem(val, i);
+        } else {
+            sum += calcItem(val, i);
+        }
+        index++;
+    }
+    return sum;
 }
