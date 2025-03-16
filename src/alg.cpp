fccd3be492ddf7bfa013f3111351b2f58cf3de4e
Misha Stokitch <119692913+Michael-Stokich@users.noreply.github.com>
2025-03-16 13:39:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,15 +5,15 @@
 
 double pown(double value, uint16_t n) {
   double result = 1.0;
-  for (uint16_t i = 0; i < n; i++) {
+  for (uint16_t i = 0; i < n; ++i) {
     result *= value;
   }
   return result;
 }
 
 uint64_t fact(uint16_t n) {
-  uint64_t result = 1.0;
-  for (uint16_t i = 1; i <= n; i++) {
+  uint64_t result = 1;
+  for (uint16_t i = 2; i <= n; ++i) {
     result *=i;
   }
   return result;
@@ -24,8 +24,8 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-    double sum = 1.0;
-    for (uint16_t i = 1; i < count; ++i) {
+    double sum = 0.0;
+    for (uint16_t i = 0; i <= count; ++i) {
         sum += calcItem(x, i);
     }
     return sum;
