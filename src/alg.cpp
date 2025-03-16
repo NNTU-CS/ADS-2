b57edbd58b35d0284528de69c8e5bb121467239d
Yana Medvedeva <134713030+Tosh1ii@users.noreply.github.com>
2025-03-16 10:10:55 +0300

    Update alg.cpp v5

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,7 +12,7 @@ double pown(double value, uint16_t n) {
 
 uint64_t fact(uint16_t n) {
   uint64_t result = 0;
-  for (uint16_t i = 1; i <= n; i++) {
+  for (uint16_t i = 1; i < (n + 1); i++) {
         result += i;
   }
   return result;
@@ -24,7 +24,7 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
   uint64_t result = 1;
-  for (uint16_t i = 1; i <= count; i++) {
+  for (uint16_t i = 1; i < (count + 1); i++) {
     result += calcItem(x, i);
   }
   return result;
@@ -32,7 +32,7 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
   uint64_t result = x;
-  for (uint16_t i = 3, i <= count; i += 2) {
+  for (uint16_t i = 3, i < (count + 1); i += 2) {
     result += calcItem(x, (2 * i - 1) );
   }
   return result;
@@ -40,7 +40,7 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
   uint64_t result = 1;
-  for (uint16_t i = 2, i <= count; i += 2) {
+  for (uint16_t i = 2, i < (count + 1); i += 2) {
     result += pown(-1, i - 1) * calcItem(x, (2 * i - 2));
   }
   return result;
