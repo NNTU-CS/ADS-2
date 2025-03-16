6ed24eae3014d5e4499a76aacb7d115c72fedb02
tcctvv <anastasiazhiharewa@yandex.ru>
2025-03-16 21:42:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@
 
 double pown(double value, uint16_t n) {
     double res = 1;
-    for (int i = 0; i <= n; ++i) {
+    for (uint16_t i = 0; i <= n; ++i) {
         res *= value;
     }
     return res;
@@ -16,28 +16,28 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
-    return (pown(x, n) / fact(n));
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
     double sum = 1;
-    for (int i = 0; i <= count; ++i) {
-        sum += calcItem(x, i);
+    for (uint16_t i = 0; i <= count; ++i) {
+        sum += pown(x, i) / fact(i);
     }
     return sum;
 }
 
 double sinn(double x, uint16_t count) {
     double res = x;
-    for (int i = 1; i < count; i++) {
-        res += calcItem(x, 2 * i + 1)* pown(-1, i % 2);
+    for (uint16_t i = 1; i < count; i++) {
+        res += pown(-1, i) * pown(x, 2 * i + 1) / fact(2 * i + 1);
     }
     return res;
 }
 
 double cosn(double x, uint16_t count) {
     double res = 1;
-    for (int i = 1; i < count; i++) {
+    for (uint16_t i = 1; i < count; i++) {
         res += calcItem(x, 2 * i) * pown(-1, i % 2);
     }
     return res;
