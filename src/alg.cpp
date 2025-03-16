5c7cb568979c268d68636a7e893968af42860814
Tripleez <art_khorosh@vk.com>
2025-03-16 21:16:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,31 +4,27 @@
 
 
 double pown(double value, uint16_t n) {
-  if (value == 0) {
-      return 1;
-  }  
-  for (int i = 1; i<n ; i++){
-    value *= value;
+  double val = 1;
+  for (int i = 1; i <= n ; i++) {
+    val *= value;
   }
-  return value;
+  return val;
 }
 
 uint64_t fact(uint16_t n) {
   if (n == 0) {
       return 1;
   }
-  else {
-      return n * fact(n - 1);
-  }
+  return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-  return (pown(x, n) / fact(x));
+  return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
   double e = 1;
-  for (int i = 1; i <= count; i++){
+  for (int i = 1; i <= count; i++) {
       e += calcItem(x, i);
   }
   return e;
@@ -36,7 +32,7 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
   double s = x;
-  for (int i = 1; i < count; i++){
+  for (int i = 1; i < count; i++) {
       s += calcItem(x, 2 * i + 1)* pown(-1, i % 2);
   }
   return s;
@@ -44,7 +40,7 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
   double c = 1;
-  for (int i = 1; i < count; i++){
+  for (int i = 1; i < count; i++) {
       c += calcItem(x, 2 * i) * pown(-1, i % 2);
   }
   return c;
