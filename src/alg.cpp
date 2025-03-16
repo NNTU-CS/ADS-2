90dd3b47f0e9b6b1984bda8d63ad4f710e59e77f
Nastya3456-fed <Nasty3256@yandex.ru>
2025-03-16 18:30:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -45,10 +45,6 @@ double sinn(double x, uint16_t count) {
 }
 
 double cosn(double x, uint16_t count) {
-  
-  if (count == 0) {
-    return 1.0;
-  }
   double itog = 0.0;
   for (uint16_t n = 0; n < count; ++n) {
     itog += (n % 2 == 0 ? 1 : -1) * pow(x, 2 * n) / fact(2 * n);
