15145cd164734e96c69ce554f9577a48017c1f6d
letmebleedout <199362693+letmebleedout@users.noreply.github.com>
2025-03-16 04:16:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,7 @@
 #include "alg.h"
 
 
-double pow(double value, uint16_t n) {
+double pown(double value, uint16_t n) {
   double result = 1.0;
   for (uint16_t i = 0; i < n; ++i) {
     result *= value;
