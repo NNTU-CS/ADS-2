a413b947e44ad19cf152da44a4c6b1d8bba2f19f
Burkov06 <s.burkov.2006@gmail.com>
2025-03-16 18:00:34 +0300

    ADS2alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,45 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+ double result = 1.0;
+    for (uint16_t i = 0; i < n; i++) {
+        result *= value;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0.0;
+    for (uint16_t n = 0; n < count; n++) {
+        sum += calcItem(x, n);
+    }
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0.0;
+    for (uint16_t n = 0; n < count; n++) {
+        sum += (n % 2 == 0 ? 1 : -1) * calcItem(x, 2 * n + 1);
+    }
+    return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0.0;
+    for (uint16_t n = 0; n < count; n++) {
+        sum += (n % 2 == 0 ? 1 : -1) * calcItem(x, 2 * n);
+    }
+    return sum;
 }
