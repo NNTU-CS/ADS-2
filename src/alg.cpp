f89dfe3c9dbe4177a54b494beeefb009a7e3db0a
tcctvv <anastasiazhiharewa@yandex.ru>
2025-03-16 21:35:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,54 +2,43 @@
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-    double result = 1.0;
-    for (uint16_t i = 0; i < n; ++i) {
-        result *= value;
+    double res = 1;
+    for (int i = 0; i <= n; ++i) {
+        res *= value;
     }
-    return result;
+    return res;
 }
 
 uint64_t fact(uint16_t n) {
     if (n == 0) return 1;
-    uint64_t result = 1;
-    for (uint16_t i = 1; i <= n; ++i) {
-        result *= i;
-    }
-    return result;
+    return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-    return pown(x, n) / fact(n);
+    return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-    double sum = 0.0;
-    for (uint16_t i = 0; i < count; ++i) {
+    double sum = 1;
+    for (int i = 0; i <= count; ++i) {
         sum += calcItem(x, i);
     }
     return sum;
 }
 
 double sinn(double x, uint16_t count) {
-    double sum = 0.0;
-    for (uint16_t n = 0; n < count; ++n) {
-        double sign = (n % 2 == 0) ? 1.0 : -1.0;
-        double numerator = pown(x, 2*n + 1);
-        uint64_t denominator = fact(2*n + 1);
-        sum += sign * (numerator / denominator);
+    double res = x;
+    for (int i = 1; i < count; i++) {
+        res += calcItem(x, 2 * i + 1)* pown(-1, i % 2);
     }
-    return sum;
+    return res;
 }
 
 double cosn(double x, uint16_t count) {
-    double sum = 0.0;
-    for (uint16_t n = 0; n < count; ++n) {
-        double sign = (n % 2 == 0) ? 1.0 : -1.0;
-        double numerator = pown(x, 2*n);
-        uint64_t denominator = fact(2*n);
-        sum += sign * (numerator / denominator);
+    double res = 1;
+    for (int i = 1; i < count; i++) {
+        res += calcItem(x, 2 * i) * pown(-1, i % 2);
     }
-    return sum;
+    return res;
 }
