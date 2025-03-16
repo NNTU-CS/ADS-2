6abb2a8008cfc16620407fc2e3681069726ff206
BushSasha <sashabush2006@gmail.com>
2025-03-17 01:42:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,61 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double result = 1.0;
+    for (uint16_t i = 0; i < n; ++i) {
+        result *= value;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n == 0 || n == 1) return 1;
+    uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  double result =1.0;
+  for (uint16_t i = 0; i<n; ++i){
+    result*=x;
+  }
+  for (uint16_t i = 1; i <=n; ++i){
+    result/=i;
+  }
+  return result;
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double summ = 0.0;
+    for (uint16_t n = 0; n < count; ++n) {
+        summ += calcItem(x, n);
+    }
+    return summ;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double summ = 0.0;
+  double c = x;
+  int sym =1;
+  for (uint16_t n = 0; n <= count; ++n) {
+    summ += sym * c;
+    c*= x*x / ((2*n) * (2*n + 1));
+    sym = -sym;
+  }
+  return summ;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double summ = 0.0;
+  double c = 1.0;
+  int sym =1;
+  for (uint16_t n = 0; n <= count; ++n) {
+    summ += sym * c;
+    c*= x*x / ((2*n - 1) * (2*n);
+    sym = -sym;
+  }
+  return summ;
 }
