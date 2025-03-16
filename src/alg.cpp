a3bf62a391128b43f9ec3cd09baa26d53da35940
eliza3455 <catra7902@gmail.com>
2025-03-16 10:58:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,58 +2,59 @@
 #include <cmath>
 #include <iostream>
 
-double pown(double num, uint16_t power) {
-    double base = num;
-    for (uint16_t i = 2; i <= power; ++i) {
-        num *= base;
-    }
-    return num;
+
+double pown(double value, uint16_t n) {
+	double value2 = value;
+	for (uint16_t i = 2; i <= n; ++i) {
+		value *= value2;
+	}
+	return value;
 }
 
-uint64_t fact(uint16_t num) {
-    if (num == 1) {
-        return 1;
-    } else {
-        return fact(num - 1) * num;
-    }
+uint64_t fact(uint16_t n) {
+	if (n == 1) {
+		return 1;
+	} else {
+		return fact(n-1) * n;
+	}
 }
 
-double calcItem(double val, uint16_t term) {
-    return (pown(val, term)) / (fact(term));
+double calcItem(double x, uint16_t n) {
+	return (pown(x, n)) / (fact(n));
 }
 
-double expn(double val, uint16_t terms) {
-    double result = 1;
-    for (uint16_t i = 1; i <= terms; ++i) {
-        result += calcItem(val, i);
-    }
-    return result;
+double expn(double x, uint16_t count) {
+	double e = 1;
+	for (uint16_t i = 1; i <= count; ++i) {
+		e += calcItem(x, i);
+	}
+	return e;
 }
 
-double sinn(double val, uint16_t terms) {
-    double sum = 0;
-    uint16_t index = 1;
-    for (uint16_t i = 1; i <= terms * 2; i += 2) {
-        if (index % 2 == 0) {
-            sum -= calcItem(val, i);
-        } else {
-            sum += calcItem(val, i);
-        }
-        index++;
-    }
-    return sum;
+double sinn(double x, uint16_t count) {
+	double sin = 0;
+	uint16_t k = 1;
+	for (uint16_t i = 1; i <= count*2; i+=2) {
+		if (k % 2 == 0) {
+			sin -= calcItem(x, i);
+		} else {
+			sin += calcItem(x, i);
+		}
+		k++;
+	}
+	return sin;
 }
 
-double cosn(double val, uint16_t terms) {
-    double sum = 1;
-    uint16_t index = 1;
-    for (uint16_t i = 2; i < terms * 2; i += 2) {
-        if (index % 2 != 0) {
-            sum -= calcItem(val, i);
-        } else {
-            sum += calcItem(val, i);
-        }
-        index++;
-    }
-    return sum;
+double cosn(double x, uint16_t count) {
+	double cos = 1;
+	uint16_t k = 1;
+	for (uint16_t i = 2; i < count*2; i += 2) {
+		if (k % 2 != 0) {
+			cos -= calcItem(x, i);
+		} else {
+			cos += calcItem(x, i);
+		}
+		k++;
+	}
+	return cos;
 }
