1bd262d1c76bb161f21680aee6f06f51b79cdf96
Spectxr <leeibov@gmail.com>
2025-03-16 20:48:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,53 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double res = 1.0;
+  for (uint16_t i = 0; i < n; ++i) {
+      res *= value;
+    }
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint16_t res = 1;
+  for (uint16_t i = 1; i <= n; ++i) {
+    res *= i;
+  }
+  return res;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    sum += calcItem(x, n);
+  }
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    double term = pown(x, 2 * n + 1) / fact(2 * n + 1);
+    if (n % 2 != 0) {
+      term = -term;
+    }
+    sum += term;
+  }
+  return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    double term = pown(x, 2 * n) / fact(2 * n);
+    if (n % 2 != 0) {
+      term = -term;
+    }
+    sum += term;
+  }
+  return sum;
 }
