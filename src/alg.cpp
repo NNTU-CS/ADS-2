b1054c590d02a27103e9669cfe05b77692b39d03
kkkxd <kovalepik@gmail.com>
2025-03-16 21:19:27 +0300

    ads2.2

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,7 +7,7 @@ double pown(double value, uint16_t n) {
   double result = 1.0;
   for (uint16_t i = 0; i < n; ++i) {
     result *= value;
-  }    
+  }
   return result;
 }
 
@@ -15,7 +15,7 @@ uint64_t fact(uint16_t n) {
   uint64_t result = 1;
   for (uint16_t i = 2; i <= n; ++i) {
     result *= i;
-  }    
+  }
   return result;
 }
 
@@ -27,7 +27,7 @@ double expn(double x, uint16_t count) {
   double result = 0.0;
   for (uint16_t i = 0; i < count; ++i) {
     result += calcItem(x, i);
-  }    
+  }
   return result;
 }
 
@@ -36,7 +36,7 @@ double sinn(double x, uint16_t count) {
   for (uint16_t i = 0; i < count; ++i) {
     double term = pown(-1, i) * pown(x, 2 * i + 1) / fact(2 * i);
     result += term
-  }      
+  }
   return result;
 }
 
@@ -45,6 +45,6 @@ double cosn(double x, uint16_t count) {
   for (uint16_t i = 0; i < count; ++i) {
     double term = pown(-1, i) * pown(x, 2 * i) / fact(2 * i);
     result += term;
-  }    
+  }
   return result;
 }
