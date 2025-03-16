492c20d6f0e51ee9c885042b968c5a26fa6fff94
t7cel <bachinalva@gmail.com>
2025-03-16 17:46:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,30 +11,31 @@ double pown(double value, uint16_t n) {
     return result;
 }
 
-uint64_t fact(uint16_t n) {
+uint64_t fact(unsigned int n) {
+  if (n == 0) return 1;
   uint64_t result = 1;
-    for (uint16_t i = 2; i <= n; ++i) {
+    for (unsigned int i = 2; i <= n; ++i) {
         result *= i;
     }
     return result;
 }
 
-double calcItem(double x, uint16_t n) {
-  return pown(x, n) / fact(n);
+double calcItem(double x, uint32_t n) {
+  return pown(x, n) / (double)fact(n);
 }
 
-double expn(double x, uint16_t count) {
+double expn(double x, uint32_t count) {
   double result = 0.0;
-    for (uint16_t i = 0; i < count; ++i) {
+    for (uint32_t i = 0; i < count; ++i) {
         result += calcItem(x, i);
     }
     return result;
 }
 
-double sinn(double x, uint16_t count) {
+double sinn(double x, uint32_t count) {
   double result = 0.0;
-    for (uint16_t i = 0; i < count; ++i) {
-        double term = (i % 2 == 0 ? 1.0 : -1.0) * pown(x, 2 * i + 1) / fact(2 * i + 1);
+    for (uint32_t i = 0; i < count; ++i) {
+        double term = (i % 2 == 0 ? 1.0 : -1.0) * pown(x, 2 * i + 1) / (double)fact(2 * i + 1);
         result += term;
     }
     return result;
