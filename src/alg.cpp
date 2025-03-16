d9cd931d27ade552c278039d68e88b6f65fd8929
Nastya3456-fed <Nasty3256@yandex.ru>
2025-03-16 16:11:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,50 @@
 // Copyright 2022 NNTU-CS
-#include <cstdint>
-#include "alg.h"
+#include <math.h>
+#include <stdio.h>
 
+#include <iostream>
+#include <cstdint>
 
 double pown(double value, uint16_t n) {
-  return 0.0;
-}
+  double itog=1.0;
+  for (uint16_t i = 0; i < n; ++i) {
+    itog *= value;
+  }
+  return itog;
+ }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint16_t itog = 1;
+  for (uint16_t i = 1; i <= n; ++i) {
+    itog *= i;
+  }
+  return itog;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return (pow(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double itog = 1.0;
+  for (uint16_t n = 1; n < count; ++n) {
+    itog += pow(x, n) / fact(n);
+  }
+  return itog;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double itog = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    itog += pow(-1, n) * pow(x, 2 * n + 1) / fact(2 * n + 1);
+  }
+  return itog;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double itog = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    itog += pow(-1, n) * pow(x, 2 * n) / fact(2 * n);
+  }
+  return itog;
 }
