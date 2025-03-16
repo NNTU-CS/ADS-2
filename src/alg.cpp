4e574fa0771ecc78e32e916ce5b5c0d2a0149824
YaroslavChinyakov <chinyakov777@gmail.com>
2025-03-16 22:24:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -23,12 +23,9 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-    double sum = 1.0;
-    double term = 1.0;
-
-    for (uint16_t n = 1; n < count; ++n) {
-        term *= x / n;
-        sum += term;
+    double sum = 1.0; 
+    for (uint16_t i = 1; i <= count; ++i) {
+        sum += calcItem(x, i); 
     }
     return sum;
 }
