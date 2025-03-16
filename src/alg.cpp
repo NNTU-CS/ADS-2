b740251eaec12326d32fb4efc2181db55023b38e
eliza3455 <catra7902@gmail.com>
2025-03-16 12:18:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,53 +1,43 @@
 // Copyright 2022 NNTU-CS
-#include <cmath>
-#include <iostream>
 #include <cstdint>
+#include "alg.h"
 
-
-uint16_t fact(uint16_t n) {
-    if (n == 0 || n == 1) {
-        return 1;
-    }
-    return fact(n-1) * n;
+uint64_t fact(uint16_t n) {
+  if (n == 0 || n == 1) return 1;
+  uint64_t res = 1;
+  for (uint16_t i = 2; i <= n; ++i) {
+    res *= i;
+  } return res;
 }
 
-double pown(double base, int exponent) {
-    double result = 1;
-    for (int i = 0; i < exponent; i++) {
-        result *= base;
-    }
-    return result;
+double pown(double val, uint16_t n) {
+  double result = 1.0;
+  for (uint16_t i = 0; i < n; ++i) {
+    result *= val;
+  } return result;
 }
 
 double calcItem(double x, uint16_t n) {
-    return (pown(x, n)) / (fact(n));
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-    double result = 1;
-    double term = 1;
-    for (int i = 1; i <= count; i++) {
-        term *= x/i;
-        result += term;
-    }
-    return result;
+  double sum = 0.0;
+  for (uint16_t i = 0; i <= count; ++i) {
+    sum += calcItem(x, i);
+  } return sum;
 }
-
 double sinn(double x, uint16_t count) {
-    if (x == 0) return 0;
-    double result = 0;
-    int sign = 1;
-    for (int i = 1; i <= count; i++) {
-        result += sign * (pow(x, (2*i - 1)) / fact(2*i - 1));
-        sign *= -1;
-    }
-    return result;
+  double sum = x;
+  for (uint16_t i = 1; i < count; ++i) {
+    double term = calcItem(x, 2*i + 1);
+    sum += (i%2 == 0) ? term : -term;
+  } return sum;
 }
-
 double cosn(double x, uint16_t count) {
-    double res = 0;
-    for (int i = 1; i <= count; i++) {
-        res += pown(-1, i-1) * (pown(x, (2*i - 2)) / fact((2*i - 2)));
-    }
-    return res;
+  double sum = 1.0;
+  for (uint16_t i = 1; i < count; ++i) {
+    double term = calcItem(x, 2 * i);
+    sum += (i%2 == 0) ? term : -term;
+  } return sum;
 }
