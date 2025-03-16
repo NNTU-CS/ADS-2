64b7bc8d623beb61331fc3211309166270380e5c
Nastya3456-fed <Nasty3256@yandex.ru>
2025-03-16 17:45:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,10 +11,10 @@ double pown(double value, uint16_t n) {
     itog *= value;
   }
   return itog;
- }
+}
 
 uint64_t fact(uint16_t n) {
-  uint16_t itog = 1;
+  uint64_t itog = 1;
   for (uint16_t i = 1; i <= n; ++i) {
     itog *= i;
   }
@@ -27,13 +27,16 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
   double itog = 1.0;
-  for (uint16_t n = 1; n < count; ++n) {
+  for (uint16_t n = 1; n <= count; ++n) {
     itog += pow(x, n) / fact(n);
   }
   return itog;
 }
 
 double sinn(double x, uint16_t count) {
+  if (x == 0.0){
+    return 0.0;
+  }
   double itog = 0.0;
   for (uint16_t n = 0; n < count; ++n) {
     itog += pow(-1, n) * pow(x, 2 * n + 1) / fact(2 * n + 1);
@@ -42,6 +45,9 @@ double sinn(double x, uint16_t count) {
 }
 
 double cosn(double x, uint16_t count) {
+  if (count == 0){
+    return 1.0;
+  }
   double itog = 0.0;
   for (uint16_t n = 0; n < count; ++n) {
     itog += pow(-1, n) * pow(x, 2 * n) / fact(2 * n);
