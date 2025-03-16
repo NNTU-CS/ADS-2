5a2d057c3f7b653c64f6c69fb5fd5bde0f13a21e
Egor Mitrofanov <82968933+MaybeeYou@users.noreply.github.com>
2025-03-16 20:45:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,44 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double res = 1;
+  for (uint16_t i = 0; i < n; i++) {
+    res *= value;
+  }
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t result = 1;
+  for (uint16_t i = 2; i <= n; ++i) {
+    result *= i;
+  }
+  return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double res = 1;
+  for (uint16_t i = 1; i <= count; i++) {
+    res += calcItem(x, i);
+  }
+  return res;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double res = x;  
+  for (int k = 1; k < count; k++) {
+    res += calcItem(x, 2 * k + 1) * pown(-1, k % 2);
+  }
+  return res; 
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double res = 1;
+  for (int k = 1; k < count; k++) {
+    res += calcItem(x, 2 * k) * pown(-1, k % 2);
+  }
 }
