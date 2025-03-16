d87bad50a21b0745d3a044b4371ec2c618ea6dbe
jariuock <garanin.019@gmail.com>
2025-03-16 16:23:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,47 @@
 // Copyright 2022 NNTU-CS
+#include <iostream>
 #include <cstdint>
+#include <cmath>
 #include "alg.h"
+using namespace std;
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+	double result = 0.0;
+	for (uint16_t i = 0; i <= n; ++i) {
+		result = pow(value, n);
+	}
+	return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+	if (n == 0)
+		return 1;
+	else 
+		return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+	return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+	double result = 0.0;
+	for (uint16_t i = 0; i <= count; ++i)
+		result += calcItem(x, i);
+	return result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+	double result = 0.0;
+	for (uint16_t i = 1; i <= count; ++i)
+		result += pown(-1, i - 1) * (pown(x, 2 * i - 1) / fact(2 * i - 1));
+	return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+	double result = 0.0;
+	for (uint16_t i = 1; i <= count; ++i)
+		result += pown(-1, i - 1) * (pown(x, 2 * i - 2) / fact(2 * i - 2));
+	return result;
 }
