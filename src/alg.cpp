5348f6545e9eebf2224e31e141feb8faea5607e7
GLUKERZZZ <artem2801200635@gmail.com>
2025-03-16 21:20:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,49 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-
-
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double res = 1.0;
+  for (uint16_t i = 0; i < n; ++i) {
+    res *= value;
+  }
+  return res;
 }
-
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t result = 1;
+  for (uint16_t i = 2; i <= n; ++i) {
+    result *= i;
+  }
+  return result;
 }
-
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  double res = 1.0;
+  for (uint16_t i = 1; i <= n; ++i) {
+    res *= x / i;
+  }
+  return res;
 }
-
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double res = 1.0;
+  for (uint16_t n = 1; n <= count; ++n) {
+    res += pown(x, n) / fact(n);
+  }
+  return res;
 }
-
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double res = 0.0;
+  double term = x;
+  for (uint16_t n = 0; n < count; ++n) {
+    res += term;
+    term *= -x * x / ((2 * n + 2) * (2 * n + 3));
+  }
+  return res;
 }
-
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0.0;
+  double term = 1.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    sum += term;
+    term *= -x * x / ((2 * n + 1) * (2 * n + 2));
+  }
+  return sum;
 }
