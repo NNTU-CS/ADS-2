deac06263681f3444fc52d51de8a5ab9ccd369a5
tcctvv <anastasiazhiharewa@yandex.ru>
2025-03-16 21:09:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,58 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+    double result = 1.0;
+    for(uint16_t i = 0; i < n; ++i) {
+        result *= value;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    // Для 0! = 1
+    if(n == 0) return 1;
+    uint64_t result = 1;
+    for(uint16_t i = 1; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 0.0;
+    for(uint16_t i = 0; i < count; ++i) {
+        sum += calcItem(x, i);  // x^i / i!
+    }
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 0.0;
+    for(uint16_t n = 0; n < count; ++n) {
+        // (-1)^n
+        double sign = (n % 2 == 0) ? 1.0 : -1.0;
+        // x^(2n+1)
+        double numerator = pown(x, 2*n + 1);
+        // (2n+1)!
+        uint64_t denominator = fact(2*n + 1);
+
+        sum += sign * (numerator / denominator);
+    }
+    return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 0.0;
+    for(uint16_t n = 0; n < count; ++n) {
+        double sign = (n % 2 == 0) ? 1.0 : -1.0;
+        double numerator = pown(x, 2*n);
+        uint64_t denominator = fact(2*n);
+
+        sum += sign * (numerator / denominator);
+    }
+    return sum;
 }
