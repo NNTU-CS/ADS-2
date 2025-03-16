bc866c46489eb4f5900807a7fd61a5e08c2f4af7
annChekh <anua.chekhonadskikh@mail.ru>
2025-03-16 19:10:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,49 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double res = 1.0;
+  for (uint16_t i = 0; i < n; i++) {
+    res *= value;
+  }
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t res = 1;
+  for (uint16_t i = 1; i <=n; i++) {
+    res *= i;
+  }
+  return res;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  double numer = pown(x,n);
+  uint64_t denom = fact(n);
+  return numer/denom;
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double res = 0.0;
+  for (uint16_t i = 0; i < count; i++) {
+    res += calcItem(x,i);
+  }
+  return res;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double res = 0.0;
+  for (uint16_t i = 1; i <= count; i++) {
+    double curr = pown(-1,i-1) * pown(x,2*i-1) / fact(2*i-1);
+    res += curr;
+  }
+  return res;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double res = 0.0;
+  for (uint16_t i = 1; i <= count; i++) {
+    double curr = pown(-1,i-1) * pown(x,2*i-2) / fact(2*i-2);
+    res += curr;
+  }
+  return res;
 }
