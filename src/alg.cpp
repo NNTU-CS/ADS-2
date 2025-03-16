3ba45ff336d1d878a03a1e7158d5eb78663be1f0
t7cel <bachinalva@gmail.com>
2025-03-16 18:55:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,48 +3,48 @@
 #include "alg.h"
 #include <cmath>
 
-double pown(double value, uint32_t n) {
+double pown(double value, unsigned short n) {
   double result = 1.0;
-    for (uint32_t i = 0; i < n; ++i) {
+    for (unsigned short i = 0; i < n; ++i) {
         result *= value;
     }
     return result;
 }
 
-uint64_t fact(uint32_t n) {
+uint64_t fact(unsigned short n) {
   if (n == 0) return 1;
   uint64_t result = 1;
-    for (uint32_t i = 2; i <= n; ++i) {
+    for (unsigned short i = 2; i <= n; ++i) {
         result *= i;
     }
     return result;
 }
 
-double calcItem(double x, uint32_t n) {
+double calcItem(double x, unsigned short n) {
   return pown(x, n) / (double)fact(n);
 }
 
-double expn(double x, uint32_t count) {
+double expn(double x, unsigned short count) {
   double result = 0.0;
-    for (uint32_t i = 0; i < count; ++i) {
+    for (unsigned short i = 0; i < count; ++i) {
         result += calcItem(x, i);
     }
     return result;
 }
 
-double sinn(double x, uint32_t count) {
+double sinn(double x, unsigned short count) {
   double result = 0.0;
-    for (uint32_t i = 0; i < count; ++i) {
-        double term = ((i % 2 == 0) ? 1.0 : -1.0) * pown(x, 2 * i + 1) / (double)fact(static_cast<uint32_t>(2 * i + 1));
+    for (unsigned short i = 0; i < count; ++i) {
+        double term = ((i % 2 == 0) ? 1.0 : -1.0) * pown(x, 2 * i + 1) / (double)fact(static_cast<unsigned short>(2 * i + 1));
         result += term;
     }
     return result;
 }
 
-double cosn(double x, uint32_t count) {
+double cosn(double x, unsigned short count) {
   double result = 0.0;
-    for (uint32_t i = 0; i < count; ++i) {
-        double term = ((i % 2 == 0) ? 1.0 : -1.0) * pown(x, 2 * i) / (double)fact(static_cast<uint32_t>(2 * i));
+    for (unsigned short i = 0; i < count; ++i) {
+        double term = ((i % 2 == 0) ? 1.0 : -1.0) * pown(x, 2 * i) / (double)fact(static_cast<unsigned short>(2 * i));
         result += term;
     }
     return result;
