f47c9f0558389180604e9f217f6fad67a915a8a6
methevodichka <145225062+methevodichka@users.noreply.github.com>
2025-03-17 20:38:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -41,6 +41,7 @@ double sinn(double x, uint16_t count) {
   double res = 0.0;
   for (uint16_t n = 0; n <= count; ++n) {
     res += calcItem(x, n);
+  }
   for (uint16_t n = 1; n <= count; ++n) {
         res += pown(-1.0, n - 1) * calcItem(x, (2 * n - 1));
   }
