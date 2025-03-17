edba692d912d10dc6475d3739d3c9cf5ba9c92c2
BushSasha <sashabush2006@gmail.com>
2025-03-17 18:20:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,16 +12,15 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-  if (n == 0 || n == 1) return 1;
-    uint64_t result = 1;
-    for (uint16_t i = 2; i <= n; ++i) {
-        result *= i;
-    }
-    return result;
+  uint64_t result = 1;
+  for (uint16_t i = 2; i <= n; ++i) {
+    result *= i;
+  }
+  return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  double result =1.0;
+  double result = 1.0;
   for (uint16_t i = 0; i < n; ++i) {
     result *= x;
   }
@@ -33,20 +32,20 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
   double summ = 0.0;
-    for (uint16_t n = 0; n < count; ++n) {
-        summ += calcItem(x, n);
-    }
-    return summ;
+  for (uint16_t n = 0; n < count; ++n) {
+    summ += calcItem(x, n);
+  }
+  return summ;
 }
 
 double sinn(double x, uint16_t count) {
   double summ = 0.0;
   double c = x;
-  int sym = 1;
+  int symb = 1;
   for (uint16_t n = 0; n <= count; ++n) {
-    summ += sym * c;
+    summ += symb * c;
     c *= x * x / ((2 * n) * (2 * n + 1));
-    sym = -sym;
+    symb = -symb;
   }
   return summ;
 }
@@ -54,11 +53,11 @@ double sinn(double x, uint16_t count) {
 double cosn(double x, uint16_t count) {
   double summ = 0.0;
   double c = 1.0;
-  int sym = 1;
+  int symb = 1;
   for (uint16_t n = 0; n <= count; ++n) {
-    summ += sym * c;
+    summ += symb * c;
     c *= x * x / ((2 * n - 1) * (2 * n));
-    sym = -sym;
+    symb = -symb;
   }
   return summ;
 }
