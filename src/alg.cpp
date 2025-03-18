0a9a8ce38ddc97e45e52a551b4d209f05f4cee11
Kirill115fz <kirillkuznecov716@gmail.com>
2025-03-18 12:25:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -17,7 +17,7 @@ double pown(double value, uint16_t n) {
 
 uint64_t fact(uint16_t n) {
     uint64_t res = 1;
-    for (uint16_t i = 2; i <= n; ++i) { 
+    for (uint16_t i = 2; i <= n; ++i) {
         res *= i;
     }
     return res;
@@ -58,13 +58,9 @@ double sinn(double x, uint16_t count) {
 }
 
 double cosn(double x, uint16_t count) {
-    double res = 1.0;
-    double term = 1.0;
-    double x_squared = -x * x;
-
-    for (uint16_t i = 1; i <= count; ++i) {
-        term *= x_squared / static_cast<double>((2 * i - 1) * (2 * i));
-        res += term;
-    }
-    return res;
+  double res = 0.0;
+  for (int i = 1; i <= count; i++)
+    res += pown(-1.0, i - 1) * pown(x, 2 * i - 2) /
+           static_cast<double>(fact(2 * i - 2));
+  return res;
 }
