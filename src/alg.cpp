312b2dd5f74b3de1028909ee4a655b17e7751a63
ossicc <ossic8881@gmail.com>
2025-03-22 17:40:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -18,7 +18,7 @@ uint64_t fact(uint16_t n) {
   if (n == 0) {
     return 1;
   }
-  uint16_t q = 1;
+  uint64_t q = 1;
   for (uint16_t i = 1; i < n + 1; i++) {
     q *= i;
   }
@@ -39,16 +39,16 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
   double s = x;
-  for (uint16_t i = 1; i < count + 1; i++) {
-    s += pown(-1, count - 1) * (pown(x, 2 * count - 1) / fact(2 * count - 1));
+  for (uint16_t i = 2; i < count + 1; i++) {
+    s += pown(-1, i - 1) * (pown(x, 2 * i - 1) / fact(2 * i - 1));
   }
   return s;
 }
 
 double cosn(double x, uint16_t count) {
   double s = 1;
-  for (uint16_t i = 1; i < count + 1; i++) {
-    s += pown(-1, i) * pown(x, 2 * i - 2) / fact(2 * i - 2);
+  for (uint16_t i = 2; i < count + 1; i++) {
+    s += pown(-1, i + 1) * pown(x, 2 * i - 2) / fact(2 * i - 2);
   }
   return s;
 }
