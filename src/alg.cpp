22c8b59c3bc08c9668690c8ea3c002e53b76891b
ossicc <ossic8881@gmail.com>
2025-03-22 16:57:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,51 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  if (n == 0) {
+    return 1;
+  }
+  double q = 1;
+  for (uint16_t i = 1; i < n + 1; i++) {
+    q = q * value;
+  }
+  return q; 
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n == 0) {
+    return 1;
+  }
+  uint16_t q = 1;
+  for (uint16_t i = 1; i < n + 1; i++) {
+    q *= i;
+  }
+  return q;
 }
 
-double calcItem(double x, uint16_t n) {
-  return 0.0;
+double calcItem(double x, uint16_t n) { 
+  return pown(x, n) / fact(n); 
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 1;
+  for (uint16_t i = 1; i < count + 1; i++) {
+    sum += (pown(x,i)/fact(i));
+  }
+  return sum; 
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
-}
+  double s=x;
+  for (uint16_t i = 1; i < count + 1; i++) {
+    s += pown(-1, count - 1)*(pown(x,2*count-1)/ fact(2 * count - 1));
+  }
+  return s;
+ }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double s = 1;
+  for (uint16_t i = 1; i < count + 1; i++) {
+    s += pown(-1, i) * pown(x, 2 * i - 2) / fact(2 * i - 2);
+  }
+  return s; 
 }
