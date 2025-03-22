2c1fa0fd5121ebb1976784a9b5d1c74bb65a6ee3
liguhska <masharostenko@mail.ru>
2025-03-22 14:09:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -24,13 +24,11 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-  double sum = 0.0;
-  double first = 1.0;
-  for (uint16_t n = 0; n < count; n++) {
-    sum += first;
-    first *= x / (n + 1);
-  }
-  return sum;
+    double sum = 1.0;
+    for (uint16_t n = 1; n <= count; n++) {
+        sum += calcItem(x, n);
+    }
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
