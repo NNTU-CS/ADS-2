1b10e6a96fad8d19c4ddbecd0b0ba52ee7421c88
egorkann <minicraftnn@mail.ru>
2025-03-23 16:45:13 +0300

    ads_2.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,49 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2025 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-  return 0.0;
+    if(n == 0){
+        return 1;
+    }
+    double z = 1;
+    for(uint16_t i = 1; i < n + 1; i++){
+        z = z * value;
+    }
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    uint64_t z = 1;
+    for (uint16_t i = 1; i <= n; ++i) {
+        z *= i;
+    }
+    return z;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 1; 
+    for (uint16_t n = 1; n < count; ++n) {
+        sum += calcItem(x, n);
+    }
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double sum = x;
+    for (uint16_t n = 2; n < count; n++) {
+        sum += pown(-1, n - 1) * pown(x, 2*n - 1) / fact(2*n - 1);
+    }
+    return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 1;
+    for (uint16_t n = 2; n < count + 1; n++) {
+        sum += pown(-1, n + 1) * pown(x, 2*n - 2) / fact(2*n - 2);
+    }
+    return sum;
 }
