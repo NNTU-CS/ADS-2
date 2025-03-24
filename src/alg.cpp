0153a1077e1f258e1d6abdf965f4988c1f77a4f9
ettennad <a.derlukova@mail.ru>
2025-03-24 23:57:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -27,9 +27,11 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-    double sum = 0.0;
-    for (uint16_t n = 0; n < count; ++n) {
-        sum += pown(x, n) / static_cast<double>(fact(n));
+    double sum = 1.0;
+    double term = 1.0;
+    for (uint16_t n = 1; n < count; ++n) {
+        term *= x / n;
+        sum += term;
     }
     return sum;
 }
