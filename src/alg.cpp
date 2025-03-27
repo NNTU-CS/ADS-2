a0bca598ce3eab29a421eb01b7eb32f9e25bc8da
egorkann <minicraftnn@mail.ru>
2025-03-27 18:09:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,47 +3,51 @@
 #include "alg.h"
 
 double pown(double value, uint16_t n) {
-    double result = 1.0;
-    for (uint16_t i = 0; i < n; ++i) {
-        result *= value;
-    }
-    return result;
+  if (n == 0) {
+    return 1;
+  }
+  double w = 1;
+  for (uint16_t i = 1; i < n + 1; i++) {
+   w = w * value;
+  }
+  return w;
 }
 
 uint64_t fact(uint16_t n) {
-    uint64_t result = 1;
-    for (uint16_t i = 1; i <= n; ++i) {
-        result *= i;
-    }
-    return result;
+  if (n == 0) {
+    return 1;
+  }
+  uint64_t w = 1;
+  for (uint16_t i = 1; i < n + 1; i++) {
+ w *= i;
+  }
+  return w;
 }
 
 double calcItem(double x, uint16_t n) {
-    return pown(x, n) / fact(n);
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-    double sum = 1.0; 
-    for (uint16_t n = 1; n < count; ++n) {
-        sum += calcItem(x, n);
-    }
-    return sum;
+  double sum = 1;
+  for (uint16_t n = 1; n < count + 1; n++) {
+ sum += pown(x, n) / fact(n);
+  }
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
-    double sum = 0.0;
-    for (uint16_t n = 0; n < count; ++n) {
-        double term = pown(-1, n) * pown(x, 2*n + 1) / fact(2*n + 1);
-        sum += term;
-    }
-    return sum;
+  double w = x;
+  for (uint16_t n = 2; n < count + 1; n++) {
+ w += pown(-1, n - 1) * (pown(x, 2 * n - 1) / fact(2 * n - 1));
+  }
+  return w;
 }
 
 double cosn(double x, uint16_t count) {
-    double sum = 0.0;
-    for (uint16_t n = 0; n < count; ++n) {
-        double term = pown(-1, n) * pown(x, 2*n) / fact(2*n);
-        sum += term;
-    }
-    return sum;
+  double w = 1;
+  for (uint16_t n = 2; n < count + 1; n++) {
+ w += pown(-1, n + 1) * pown(x, 2 * n - 2) / fact(2 * n - 2);
+  }
+  return w;
 }
