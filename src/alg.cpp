554d3025d4dee446bfa8ad65f4dbe5e44691d2f6
EsheTotPri <eshetotpridurok@gmail.com>
2025-03-29 17:15:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,48 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+	double res = 1;
+	for (uint16_t i = 0; i < n; i++) {
+        res*=value;
+    }
+    return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    uint64_t res = 1;
+    for (uint16_t i = n; i > 0; i--) {
+        res *= i;
+    }
+	return res;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    double Xn = pown(x, n);
+    uint64_t N = fact(n);
+    double res = Xn / N;
+    return res;
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double e = 1.0;
+    for (uint16_t i = 1; i <= count; i++) {
+        e += calcItem(x, i);
+    }
+    return e;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double sinx = 0;
+    for (uint16_t n = 1; n <= count; n++) {
+        sinx+= pown(-1, n-1) * calcItem(x, 2*n-1);
+    }
+    return sinx;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double cosx = 0;
+    for (uint16_t n = 1; n <= count; n++) {
+        cosx+= pown(-1,n-1) * calcItem(x, 2*n-2);
+    }
+    return cosx;
 }
