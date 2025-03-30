ed804673a28553c895fed38817a13b08c31f1091
Oleg <byosipow.second@gmail.com>
2025-03-30 14:12:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,26 +3,51 @@
 #include "alg.h"
 
 
+
 double pown(double value, uint16_t n) {
-  return 0.0;
+    double result = 1.0;
+    for (uint16_t i = 0; i < n; i++) {
+        result *= value;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    return pown(x, n) / (double)fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t i = 0; i < count; i++) {
+        result += calcItem(x, i);
+    }
+    return result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t i = 0; i < count; i++) {
+        uint16_t term = 2 * i + 1;
+        double sign = (i % 2 == 0) ? 1.0 : -1.0;
+        result += sign * pown(x, term) / (double)fact(term);
+    }
+    return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t i = 0; i < count; i++) {
+        uint16_t term = 2 * i;
+        double sign = (i % 2 == 0) ? 1.0 : -1.0;
+        result += sign * pown(x, term) / (double)fact(term);
+    }
+    return result;
 }
