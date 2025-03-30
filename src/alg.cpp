49300c075c2e481bfa742ed9a788229828243f9f
ratatyq <n.mantorov@yandex.ru>
2025-03-30 14:03:55 +0300

    alg2.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,27 +2,54 @@
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double res = 1.0;
+  for (uint16_t i = 0; i < n; i++) {
+    res *= value;
+  }
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t res = 1;
+  for (uint16_t i = n; i > 0; i--) {
+    res *= i;
+  }
+  return res;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double s = 1.0;
+  for (uint16_t n = 1; n <= count; n++) {
+    s += calcItem(x, n);
+  }
+  return s;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double s = 0.0;
+  for (uint16_t k = 0; k < count; k++) {
+    double t = calcItem(x, 2 * k + 1);
+    if (k % 2 == 1) {
+      t = -t;
+    }
+    s += t;
+  }
+  return s;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double s = 0.0;
+  for (uint16_t k = 0; k < count; k++) {
+    double t = calcItem(x, 2 * k);
+    if (k % 2 == 1) {
+      t = -t;
+    }
+    s += t;
+  }
+  return s;
 }
