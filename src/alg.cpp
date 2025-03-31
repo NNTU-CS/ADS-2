9846410380ff9cc9047495a46503b685ab92a2aa
Андрей <128510774+andrey-99@users.noreply.github.com>
2025-03-31 19:56:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -40,7 +40,7 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
   double res = x;
-  for (int i = 2; i < count; i++) {
+  for (int i = 2; i <= count; i++) {
     res += pown(-1, i-1) * calcItem(x, 2 * i - 1);
   }
   return res;
@@ -48,7 +48,7 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
   double res = 1;
-  for (int i = 2; i < count; i++) {
+  for (int i = 2; i <= count; i++) {
     res += pown(-1, i-1) * calcItem(x, 2 * i - 2);
   }
   return res;
