11d796253ee36022ee066736511b773a269e835a
Андрей <128510774+andrey-99@users.noreply.github.com>
2025-03-31 18:26:38 +0300

    Update alg.cpp
    first part

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,15 +4,26 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double sum = value;
+  for (int i = 1; i < n; i++) {
+    sum *= value;
+  }
+  return sum;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n == 0) {
+    return 1;
+  }
+  for (int i = 1; i < n; i++) {
+    n *= i;
+  }
+  return n;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  double num = pown(x, n) / fact(n);
+  return num;
 }
 
 double expn(double x, uint16_t count) {
