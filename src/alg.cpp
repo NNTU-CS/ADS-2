7f5c2030c19ec3c2665b90f146774465c40c2f70
Anitone <133121791+Anitone@users.noreply.github.com>
2025-04-02 13:20:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,60 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double result = 1.0;
+  double base = value;
+  for (int i = 0; i < n; i++){
+    result *= value;
+  }
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n == 0) {
+    return 1;
+  }
+  uint64_t result = n * fact(n - 1);
+  return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double summ = 0.0;
+  for (uint16_t i = 0; i < count; i++) {
+    summ += calcItem(x, n);
+  }
+  return summ;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double summ = 0.0;
+  for (uint16_t i = 0; i < count; i++){
+    double returns;
+    if (n % 2 == 0) {
+      returns = calcItem(x, 2 * n + 1);
+    }
+    if (n % 2 == 1) {
+      returns = -calcItem(x, 2 * n + 1);
+    }
+    summ += returns;
+  }
+  return summ;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+   double summ = 0.0;
+  for (uint16_t i = 0; i < count; i++){
+    double returns;
+    if (n % 2 == 0) {
+      returns = calcItem(x, 2 * n - 2);
+    }
+    if (n % 2 == 1) {
+      returns = -calcItem(x, 2 * n - 2);
+    }
+    summ += returns;
+  }
+  return summ;
 }
