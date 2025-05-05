480e715d8b904237f76ac1998d560b6d89669aae
squirrelAf <aleckraowo@gmail.com>
2025-05-05 19:22:34 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,11 +6,11 @@
 double pown(double value, uint16_t n) {
  double result = 1.0;
  for (uint16_t i = 0; i < n; ++i) {
-   result *= value;
+  result *= value;
     }
     return result;
 }
-uint64_t fact(uint16_t n) {
+uint64_t factorial(uint16_t n) {
   double result = 1.0;
     for (uint16_t i = 2; i <= n; ++i) {
         result *= i;
@@ -18,32 +18,31 @@ uint64_t fact(uint16_t n) {
     return result;
 }
 double calcItem(double x, uint16_t n) {
-    double fact = tgamma(n + 1); 
-    double term = pow(x, n) / fact;
+    double factorial = tgamma(n + 1); 
+    double term = pow(x, n) / factorial;
     return term;
 }
-
 double expn(double x, uint16_t count) {
-   double sum = 0.0;
-   for (uint16_t n = 0; n < terms; ++n) {
-        sum += pown(x, n) / factorial(n);
+ double sum = 0.0;
+   for (uint16_t n = 0; n < term; ++n) {
+    sum += pown(x, n) / factorial(n);
     }
     return sum;
 }
 double sinn(double x, uint16_t count) {
  double sum = 0.0;
  for (uint16_t n = 0; n < count; ++n) {
-        double term = (n % 2 == 0 ? 1 : -1) * (pow(x, 2 * n + 1) / factorial(2 * n + 1));
+  double term = (n % 2 == 0 ? 1 : -1) * (pow(x, 2 * n + 1) / factorial(2 * n + 1));
         sum += term;
     }
     return sum;
 }
 double cosn(double x, uint16_t count) {
- double sum = 0.0;
- double term = 1.0; 
-    for (uint16_t n = 0; n < count; n++){
+double sum = 0.0;
+double term = 1.0;
+for (uint16_t n = 0; n < count; n++){
         sum += term;
-        term *= -x * x / ((2 * n + 1) * (2 * n + 2));
+        term *= -x*x/((2*n+1)*(2*n+2));
     }
     return sum;
 }
