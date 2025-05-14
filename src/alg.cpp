ef54e7f66649ffa211c89aaf877b88b479e6e288
nitron1108 <mja29122@gmail.com>
2025-05-14 23:38:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,58 +1,57 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
+#include <cmath>
 #include "alg.h"
 
 double pown(double value, uint16_t n) {
-  double result = 1.0;
-  for (uint16_t i = 0; i < n; ++i) {
-    result *= value;
-  }
-  return result;
+    double result = 1.0;
+    for (uint16_t i = 0; i < n; ++i) {
+        result *= value;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  uint64_t result = 1;
-  for (uint16_t i = 2; i <= n; ++i) {
-    result *= i;
-  }
-  return result;
+    uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  double item = 1.0;
-  for (uint16_t i = 1; i <= n; ++i) {
-    item *= x / i;
-  }
-  return item;
+    return pown(x, n) / static_cast<double>(fact(n));
 }
 
 double expn(double x, uint16_t count) {
-  double sum = 1.0; 
-  double term = 1.0;
-  
-  for (uint16_t n = 1; n < count; ++n) {
-    term *= x / n;
-    sum += term;
-  }
-  return sum;
+    double sum = 1.0; 
+    double term = 1.0;
+    
+    for (uint16_t n = 1; n < count; ++n) {
+        term *= x / n;
+        sum += term;
+    }
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  double sum = 0.0;
-  double term = x;
-  for (uint16_t n = 1; n <= count; ++n) {
-    sum += term;
-    term *= -x * x / ((2*n) * (2*n+1));
-  }
-  return sum;
+    double sum = 0.0;
+    double term = x;
+    
+    for (uint16_t n = 1; n <= count; ++n) {
+        sum += term;
+        term *= -x * x / ((2*n) * (2*n + 1));
+    }
+    return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  double sum = 1.0;
-  double term = 1.0;
-  for (uint16_t n = 1; n <= count; ++n) {
-    term *= -x * x / ((2*n-1) * (2*n));
-    sum += term;
-  }
-  return sum;
+    double sum = 1.0;
+    double term = 1.0;
+    
+    for (uint16_t n = 1; n <= count; ++n) {
+        term *= -x * x / ((2*n - 1) * (2*n));
+        sum += term;
+    }
+    return sum;
 }
