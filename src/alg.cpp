f4767d2ec49bdac8e679dfc2a6877a5921da9974
kuzzmina <113035696+kuzzmina@users.noreply.github.com>
2025-05-15 11:21:57 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,15 +7,15 @@ double pown(double value, uint16_t n) {
   }
   return result;
 }
-uint64_t fact(uint16_t n) {
-  uint64_t result = 1;
+double fact(uint16_t n) {
+  double result = 1.0;
   for (uint16_t i = 2; i <= n; ++i) {
     result *= i;
   }
   return result;
 }
 double calcItem(double x, uint16_t n) {
-  return pown(x, n) / static_cast<double>(fact(n));
+  return pown(x, n) / fact(n);
 }
 double expn(double x, uint16_t count) {
   double result = 0.0;
