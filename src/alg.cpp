bad4aa82ea7c3d9722cd6770d212f5d450433377
kuzzmina <113035696+kuzzmina@users.noreply.github.com>
2025-05-15 11:10:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,46 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
-#include "alg.h"
-
-
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double result = 1.0;
+  for (uint16_t i = 0; i < n; ++i) {
+    result *= value;
+  }
+  return result;
 }
-
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t result = 1;
+  for (uint16_t i = 2; i <= n; ++i) {
+    result *= i;
+  }
+  return result;
 }
-
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
-
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0.0;
+  for (uint16_t i = 0; i < count; ++i) {
+    result += calcItem(x, i);
+  }
+  return result;
 }
-
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0.0;
+  for (uint16_t i = 0; i < count; ++i) {
+    uint16_t power = 2 * i + 1;
+    double term = calcItem(x, power);
+    if (i % 2 == 0) result += term;
+    else result -= term;
+  }
+  return result;
 }
-
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0.0;
+  for (uint16_t i = 0; i < count; ++i) {
+    uint16_t power = 2 * i;
+    double term = calcItem(x, power);
+    if (i % 2 == 0) result += term;
+    else result -= term;
+  }
+  return result;
 }
