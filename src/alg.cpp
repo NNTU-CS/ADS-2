ed807c0a2cb2bafb8f9c95324a117ae7886894ff
anton <ashtanyuk@gmail.com>
2026-03-01 20:27:20 +0300

    Начальный коммит

---
+++ src/alg.cpp
@@ -0,0 +1,28 @@
+// Copyright 2022 NNTU-CS
+#include <cstdint>
+#include "alg.h"
+
+
+double pown(double value, uint16_t n) {
+  return 0.0;
+}
+
+uint64_t fact(uint16_t n) {
+  return 0.0;
+}
+
+double calcItem(double x, uint16_t n) {
+  return 0.0;
+}
+
+double expn(double x, uint16_t count) {
+  return 0.0;
+}
+
+double sinn(double x, uint16_t count) {
+  return 0.0;
+}
+
+double cosn(double x, uint16_t count) {
+  return 0.0;
+}
