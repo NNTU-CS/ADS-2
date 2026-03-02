ad5b2edc0b486a2fed54c259215c5bb3a97b1a2b
forcixxx <cixfor868@gmail.com>
2026-03-03 00:27:41 +0300

    Ласточкин Олег ист3

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,27 +2,54 @@
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-  return 0.0;
+    double res = 1;
+    for (uint16_t i = 0; i < n; i++) {
+        res *= value;
+    }
+    return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    uint64_t res = 1;
+    for (uint16_t i = 1; i <= n; i++) {
+        res *= i;
+    }
+    return res;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 0;
+    for (uint16_t n = 0; n <= count; n++) {
+        sum += calcItem(x, n);
+    }
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 0;
+    for (uint16_t n = 0; n < count; n++) {
+        double term = pown(-1.0, n) * pown(x, 2 * n + 1) / fact(2 * n + 1);
+        sum += term;
+    }
+    return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 0;
+    for (uint16_t n = 0; n < count; n++) {
+        double term = pown(-1.0, n) * pown(x, 2 * n) / fact(2 * n);
+        sum += term;
+    }
+    return sum;
+}
+    for (uint16_t n = 0; n < count; n++) {
+        double term = pown(-1.0, n) * pown(x, 2 * n) / fact(2 * n);
+        sum += term;
+    }
+    return sum;
 }
