3e7e5bfd5c95effb7bef9d411a25fb2799b03837
Denis <zl1kestr1keez@gmail.com>
2026-03-02 10:49:52 +0300

    Сахаров Денис 25-ИСТ-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -26,7 +26,7 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
   double sum = 0;
-  for (uint16_t p = 0; p < count; p++) {
+  for (uint16_t p = 0; p <= count; p++) {
     double a = calcItem(x, p);
     sum += a;
   }
