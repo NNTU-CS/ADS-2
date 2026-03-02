0c2b424f82f2b122e58d395bce15b781a9c11522
Denis <zl1kestr1keez@gmail.com>
2026-03-02 10:36:21 +0300

    Сахаров Денис 25-ИСТ-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,27 +2,67 @@
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double rez = 1;
+  for (uint16_t p = 1; p < n; p++) {
+    rez *= value;
+  }
+  return rez;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t rez = 1;
+  for (uint16_t p = 1; p <= n; p++) {
+    rez *= p;
+  }
+  return rez;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  double chis = pown(x, n);  
+  uint64_t znam = fact(n); 
+  return chis / znam; 
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0;
+  for (uint16_t p = 0; p < count; p++) {
+    double a = calcItem(x, p);  
+    sum += a;               
+  }
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0;
+  for (uint16_t p = 0; p < count; p++) {
+    uint16_t step = p * 2 + 1;         
+    double chis = pown(x, step);    
+    uint64_t znam = fact(step);    
+    double a = chis / znam;  
+    if (p % 2 == 0) {                     
+      sum += a;                  
+    } 
+    else {                   
+      sum -= a;    
+    }
+  }  
+  return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0;
+  for (uint16_t p = 0; p < count; p++) {
+    uint16_t step = p * 2;                 
+    double chis = pown(x, step);      
+    uint64_t znam = fact(step);      
+    double a = chis / znam;    
+    if (p % 2 == 0) {                       
+      sum += a;                      
+    } 
+    else {                                   
+      sum -= a;                      
+    }
+  }
+  return sum;
 }
