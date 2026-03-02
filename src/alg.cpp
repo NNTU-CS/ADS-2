e1e710f7cdc3eabdd0aae007f168c8a807a1ae2d
patalena <lenapat0212@gmail.com>
2026-03-02 14:20:15 +0300

    Патанина Елена 25-ИСТ-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,27 +2,50 @@
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-  return 0.0;
+    double ans = 1.0;
+    for (uint16_t i = 0; i < n; ++i) {
+        ans *= value;
+    }
+    return ans;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    uint64_t ans = 1;
+    for (uint16_t i = 2; i <= n; ++i) {
+        ans *= i;
+    }
+    return ans;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double suma = 1.0;
+    for (uint16_t n = 1; n <= count; ++n) {
+        suma += calcItem(x, n);
+    }
+    return suma;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double suma = x;
+    int zn = -1;
+    for (uint16_t n = 3; n <= 2*count-1; n+=2) {
+        suma += calcItem(x, n) * zn;
+        zn = -zn;
+    }
+    return suma;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double suma = 1;
+    int zn = -1;
+    for (uint16_t n = 2; n <= 2 * count - 2; n += 2) {
+        suma += calcItem(x, n) * zn;
+        zn = -zn;
+    }
+    return suma;
 }
