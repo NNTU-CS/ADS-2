5c403ee9b02c06d5c87c2072f930bfdce2e3ad8f
patalena <lenapat0212@gmail.com>
2026-03-02 20:20:18 +0300

    Патанина Елена 25-ИСТ-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,7 +11,6 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-    if (n <= 1) return 1;
     uint64_t ans = 1;
     for (uint16_t i = 2; i <= n; ++i) {
         ans *= i;
@@ -24,8 +23,8 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-    double suma = 1.0;
-    for (uint16_t n = 1; n <= count; ++n) {
+    double suma = 0.0;
+    for (uint16_t n = 0; n <= count; ++n) {
         suma += calcItem(x, n);
     }
     return suma;
@@ -34,10 +33,9 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
     double suma = 0.0;
     int zn = 1;
-
-    for (uint16_t n = 1; n <= count; ++n) {
-        uint16_t power = 2 * n - 1; 
-        suma += zn * calcItem(x, power);
+    for (uint16_t i = 1; i <= count; ++i) {
+        uint16_t n = 2 * i - 1;
+        suma += zn * calcItem(x, n);
         zn = -zn;
     }
     return suma;
@@ -47,15 +45,9 @@ double sinn(double x, uint16_t count) {
 double cosn(double x, uint16_t count) {
     double suma = 0.0;
     int zn = 1;
-
-    for (uint16_t n = 1; n <= count; ++n) {
-        uint16_t power = 2 * n - 2;
-        if (power == 0) {
-            suma += zn * 1.0;
-        }
-        else {
-            suma += zn * calcItem(x, power);
-        }
+    for (uint16_t i = 1; i <= count; ++i) {
+        uint16_t n = 2 * i - 2;
+        suma += zn * calcItem(x, n);
         zn = -zn;
     }
     return suma;
