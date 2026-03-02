794f74f8bb4267a824c161789cad6ed8d2ac54b8
Denis <zl1kestr1keez@gmail.com>
2026-03-02 10:44:54 +0300

    Сахаров Денис 25-ИСТ-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,16 +19,16 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
-  double chis = pown(x, n);  
-  uint64_t znam = fact(n); 
-  return chis / znam; 
+  double chis = pown(x, n);
+  uint64_t znam = fact(n);
+  return chis / znam;
 }
 
 double expn(double x, uint16_t count) {
   double sum = 0;
   for (uint16_t p = 0; p < count; p++) {
-    double a = calcItem(x, p);  
-    sum += a;               
+    double a = calcItem(x, p);
+    sum += a;
   }
   return sum;
 }
@@ -36,32 +36,30 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
   double sum = 0;
   for (uint16_t p = 0; p < count; p++) {
-    uint16_t step = p * 2 + 1;         
-    double chis = pown(x, step);    
-    uint64_t znam = fact(step);    
-    double a = chis / znam;  
-    if (p % 2 == 0) {                     
-      sum += a;                  
-    } 
-    else {                   
-      sum -= a;    
+    uint16_t step = p * 2 + 1;
+    double chis = pown(x, step);
+    uint64_t znam = fact(step);
+    double a = chis / znam;
+    if (p % 2 == 0) {       
+      sum += a;
+    } else {
+      sum -= a;
     }
-  }  
+  }
   return sum;
 }
 
 double cosn(double x, uint16_t count) {
   double sum = 0;
   for (uint16_t p = 0; p < count; p++) {
-    uint16_t step = p * 2;                 
-    double chis = pown(x, step);      
-    uint64_t znam = fact(step);      
-    double a = chis / znam;    
-    if (p % 2 == 0) {                       
-      sum += a;                      
-    } 
-    else {                                   
-      sum -= a;                      
+    uint16_t step = p * 2;
+    double chis = pown(x, step);
+    uint64_t znam = fact(step);
+    double a = chis / znam;
+    if (p % 2 == 0) {         
+      sum += a;      
+    } else {
+      sum -= a;
     }
   }
   return sum;
