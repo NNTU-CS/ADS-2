517a8c77c10a2d1639ab4d33e5789380871e2502
Edelweizznick <kolia3003@yahoo.com>
2026-03-03 22:02:28 +0300

    update

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,13 +19,13 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
-    double abs_x = (x < 0.0) ? -x : x;
-    return pown(abs_x, n) / static_cast<double>(fact(n));
+    return pown(x, n) / static_cast<double>(fact(n));
 }
 
 double expn(double x, uint16_t count) {
     double sum = 0.0;
-    for (uint16_t n = 0; n < count; ++n) {
+    
+    for (uint16_t n = 0; n <= count; ++n) {
         sum += calcItem(x, n);
     }
     return sum;
@@ -33,7 +33,7 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
     double sum = 0.0;
-    for (uint16_t k = 0; k < count; ++k) {
+    for (uint16_t k = 0; k <= count; ++k) {
         uint16_t n = 2 * k + 1;
         double term = calcItem(x, n);
         if (k % 2 == 0) {
@@ -47,7 +47,7 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
     double sum = 0.0;
-    for (uint16_t k = 0; k < count; ++k) {
+    for (uint16_t k = 0; k <= count; ++k) {
         uint16_t n = 2 * k;
         double term = calcItem(x, n);
         if (k % 2 == 0) {
