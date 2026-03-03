5f6a67f420930387b569dc70fef1d27c6711390d
4yMHouDeZ <sergoart1234@gmail.com>
2026-03-03 11:46:03 +0300

    init

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,35 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  return pown(value, value, n);
+}
+
+double pown(double first, double current, uint16_t n) {
+  if (n == 1) return current;
+  else if (n == 0) return 1;
+  return pown(first, current * first, n - 1);
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n == 0) return 1;
+  return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  if (count < 0) return 0;
+  return calcItem(x, count) + expn(x, count - 1);
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  if (count < 0) return 0;
+  return pown(-1, count - 1) * calcItem(x, 2 * count - 1) + sinn(x - 2, count - 2);
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  if (count < 0) return 0;
+  return pown(-1, count - 1) * calcItem(x, 2 * count - 2) + cosn(x - 2, count - 2);
 }
