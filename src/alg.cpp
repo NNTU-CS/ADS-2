e0d0be5abfacd86d7139ea637009066dac4ad23e
4yMHouDeZ <sergoart1234@gmail.com>
2026-03-04 01:02:14 +0300

    ver 3

--- src/alg.cpp
+++ src/alg.cpp
@@ -31,7 +31,7 @@ double sinn(double x, uint16_t count) {
   uint8_t t;
   for (uint16_t n = 1; n <= count; n++) {
     if ((n - 1) % 2 == 0) t = 1;
-    else t = 1;
+    else t = -1;
 
     sum += t * calcItem(x, 2 * n - 1);
   }
@@ -43,7 +43,7 @@ double cosn(double x, uint16_t count) {
   uint8_t t;
   for (uint16_t n = 1; n <= count; n++) {
     if ((n - 1) % 2 == 0) t = 1;
-    else t = 1;
+    else t = -1;
 
     sum += t * calcItem(x, 2 * n - 2);
   }
