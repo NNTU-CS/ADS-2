56ed78505718c9765cba97f52e3e1240d2b3875d
Edelweizznick <kolia3003@yahoo.com>
2026-03-03 22:19:52 +0300

    update

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,18 +3,17 @@
 #include "alg.h"
 
 double pown(double value, uint16_t n) {
-    if (n == 0) return 1.0;
-
     double result = 1.0;
-    double base = value;
+    double base   = value;
 
     while (n > 0) {
-        if (n % 2 == 1) {
+        if (n & 1) {
             result *= base;
         }
         base *= base;
-        n /= 2;
+        n >>= 1;
     }
+
     return result;
 }
 
@@ -43,6 +42,7 @@ double sinn(double x, uint16_t count) {
     for (uint16_t k = 0; k <= count; ++k) {
         uint16_t n = 2 * k + 1;
         double term = calcItem(x, n);
+
         if (k % 2 == 0) {
             sum += term;
         } else {
@@ -57,6 +57,7 @@ double cosn(double x, uint16_t count) {
     for (uint16_t k = 0; k <= count; ++k) {
         uint16_t n = 2 * k;
         double term = calcItem(x, n);
+
         if (k % 2 == 0) {
             sum += term;
         } else {
