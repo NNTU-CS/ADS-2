7637a318ea1b20bf96fdf83a0dcf1721e50ef580
ulyanab1337-sys <ulyanab1337@gmail.com>
2026-03-05 21:38:49 +0300

    Бабушкина У.М. 25-ИСТ-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,57 @@
 // Copyright 2022 NNTU-CS
-#include <cstdint>
-#include "alg.h"
-
-
-double pown(double value, uint16_t n) {
-  return 0.0;
+double pow(double value, uint16_t n) {
+    double result = 1.0;
+    for (uint16_t i = 0; i < n; ++i) {
+        result *= value;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    if (n == 0) return 1.0;
+    return pow(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t n = 0; n < count; ++n) {
+        result += calcItem(x, n);
+    }
+    return result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t n = 1; n <= count; ++n) {
+        uint16_t power = 2 * n - 1;
+        double term = pow(x, power) / fact(power);
+        if (n % 2 == 0) {
+            result -= term;
+        } else {
+            result += term;
+        }
+    }
+    return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t n = 1; n <= count; ++n) {
+        uint16_t power = 2 * (n - 1);
+        double term = pow(x, power) / fact(power);
+        if (n % 2 == 0) {
+            result -= term;
+        } else {
+            result += term;
+        }
+    }
+    return result;
 }
