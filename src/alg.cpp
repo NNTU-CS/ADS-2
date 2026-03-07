70e0673c02ccff97ccd07ed742fa10b8ad7c3f98
anton-mp <abramova2712katya@gmail.com>
2026-03-07 18:49:15 +0300

    Update alg.cpp
    added functions

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,49 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+    double res = 1.0;
+    for (int i = 0; i < n; ++i) {
+        res *= value;
+    }
+    return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    uint64_t res = 1.0;
+    while (n > 0) {
+        res *= n;
+        --n;
+    }
+    return res;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double res = 0;
+    for (int i = 0; i <= count; ++i)
+    {
+        res += calcItem(x, i);
+    }
+    return res;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double res = 0;
+    for (int i = 1; i <=count; ++i)
+    {
+        res += ((i % 2) ? 1 : -1) * calcItem(x, 2 * i - 1);
+    }
+    return res;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double res = 0;
+    for (int i = 1; i <= count; ++i)
+    {
+        res += ((i % 2) ? 1 : -1) * calcItem(x, 2 * i - 2);
+    }
+    return res;
 }
