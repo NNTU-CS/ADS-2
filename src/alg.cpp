0929d1c09485d5bd9e481688d633e62768b8041a
eklochkova101 <260723543+eklochkova101@users.noreply.github.com>
2026-03-07 18:13:30 +0300

    Клочкова Е. В. 25-ИСТ-4-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,31 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  return n == 0 ? 1.0 : value * pown(value, n - 1);
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n <= 1) return 1;
+  uint64_t result = n * fact(n - 1);
+  return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return n == 0 ? 1.0 : pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  return count == 0 ? calcItem(x, 0) : expn(x, count - 1) + calcItem(x, count);
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  if (count == 0) return 0;
+  uint16_t n = count;
+  return sinn(x, count - 1) + (pown(-1, n - 1) * calcItem(x, 2 * n - 1));
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  if (count == 0) return 0;
+  uint16_t n = count;
+  return cosn(x, count - 1) + (pown(-1, n - 1) * calcItem(x, 2 * n - 2));
 }
