5c9b6f39ed4c0629237ae06ea0208a37bf097968
pupu08429-svg <pupu08429@gmail.com>
2026-03-09 22:53:19 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,54 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  if (n == 1) { return value; }
+  if (n == 0) { return 1.0; }
+  double k = 1.0;
+  for (uint16_t i = 0; i < n; i++) {
+    k = k * value;
+  }
+  return k;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n == 0) { return 1; }
+  uint64_t k = 1;
+  for (uint64_t i = 2; i <= n; ++i) {
+    k *= i;
+  }
+  return k;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n)/fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double s = 1.0;
+  for (uint16_t a = 1; a <= count; a++) {
+    s = s + ((pown(x, a)) / fact(a));
+  }
+  return s;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  if (x == 0) { return 0.0; }
+  double s = x;
+  double k = 2;
+  while (k <= count) {
+    s = s + (pown(-1, k-1)*calcItem(x, 2 * k - 1));
+    k = k + 1;
+  }
+  return s;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  if (x == 0) { return 1.0; }
+  double s = 1.0;
+  double k = 2;
+  while (k <= count) {
+    s = s + (pown(-1, k - 1) * calcItem(x, 2 * k - 2));
+    k = k + 1;
+  }
+  return s;
 }
