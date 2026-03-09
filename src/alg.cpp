5fe9c7698c31344b97e70cdc6f707b4c1a85c13d
onicuto <kiryauvachan@gmail.com>
2026-03-09 09:49:15 +0300

    redact alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,28 @@
 // Copyright 2022 NNTU-CS
-#include <cstdint>
 #include "alg.h"
-
+#include <cstdint>
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double result = value;
+  for (int i = 0; i < n; i++) {
+    result = result * result;
+  }
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t result = 1, a = 1;
+  for (int i = 0; i < n; i++) {
+    result = result * a;
+    a++;
+  }
+  return result;
 }
 
-double calcItem(double x, uint16_t n) {
-  return 0.0;
-}
+double calcItem(double x, uint16_t n) { return (pown(x, n) / fact(n)); }
 
-double expn(double x, uint16_t count) {
-  return 0.0;
-}
+double expn(double x, uint16_t count) { return 0.0; }
 
-double sinn(double x, uint16_t count) {
-  return 0.0;
-}
+double sinn(double x, uint16_t count) { return 0.0; }
 
-double cosn(double x, uint16_t count) {
-  return 0.0;
-}
+double cosn(double x, uint16_t count) { return 0.0; }
