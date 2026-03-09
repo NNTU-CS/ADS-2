c17442a70db4da181abde0027b5691b42a82d3fb
saq1rette <165058154+saq1rette@users.noreply.github.com>
2026-03-10 01:18:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,58 @@
-// Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
 
-
-double pown(double value, uint16_t n) {
-  return 0.0;
+double pow(double value, uint16_t n) {
+    if (n == 0) return 1.0;
+    double result = 1.0;
+    for (uint16_t i = 0; i < n; i++) {
+        result *= value;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    if (n == 0 || n == 1) return 1;
+    uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    return pow(x, n) / static_cast<double>(fact(n));
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t n = 0; n < count; n++) {
+        result += calcItem(x, n);
+    }
+    return result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t n = 1; n <= count; n++) {
+        double item = pow(x, 2*n - 1) / static_cast<double>(fact(2*n - 1));
+        if (n % 2 == 1) {  // нечет n дают +
+            result += item;
+        } else {  // чет n дают -
+            result -= item;
+        }
+    }
+    return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t n = 0; n < count; n++) {
+        double item = pow(x, 2*n) / static_cast<double>(fact(2*n));
+        if (n % 2 == 0) {  // чет n дают +
+            result += item;
+        } else {  // нечет n дают -
+            result -= item;
+        }
+    }
+    return result;
 }
