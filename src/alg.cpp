dd84b1013f89b599da98a633e9d4c1588c18a1df
nikSverdlik <niksverdlik@gmail.com>
2026-03-11 19:26:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,46 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+    double result = 1.0;
+    while (n != 0) {
+        result *= value;
+        n--;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    uint64_t facterial = 1;
+    for (uint16_t i = n; i > 0; i--) {
+        facterial *= i;
+    }
+    return facterial;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / (double)fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double expanenta = 1.0;
+    for (uint16_t i = 1; i < count + 1; i++) {
+        expanenta += calcItem(x, i);
+    }
+    return expanenta;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double sinus = 0.0;
+    for (uint16_t i = 1, n = 0; n < count; i += 2, n++) {
+        sinus += (pown(x, i) / (double)fact(i)) * pown(-1, n);
+    }
+    return sinus;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double cosinus = 0.0;
+    for (uint16_t i = 0, n = 0; n < count; i += 2, n++) {
+        cosinus += (pown(x, i) / (double)fact(i)) * pown(-1, n);
+    }
+    return cosinus;
 }
