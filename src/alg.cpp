71e570054065a8e30581237c167ec9d00f734218
Rost870 <60008611+Rost870@users.noreply.github.com>
2026-03-12 20:41:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,52 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double a=1;
+  for(u_int16_t i=0;i<n;i++){
+        a*=value;
+    }
+    return a;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n <= 1) return 1;
+  return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x,n)/fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double sum=0;
+  for(uint16_t i=0;i<count;i++){
+    sum+=calcItem(x,i);
+  }
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double sum=0;
+   for(uint16_t i = 1; i <= count; i++) {  
+        uint16_t exp = 2*i - 1;        
+        double ter = pown(x, exp) / fact(exp);
+        if(i % 2 == 1)  
+            sum += ter;
+        else            
+            sum -= ter;
+    }
+    return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 0;
+    for(uint16_t i= 0; i < count; i++) {  
+        uint16_t exp = 2*i;          
+        double ter = pown(x, exp) / fact(exp);
+        if(i % 2 == 0)  
+            sum += ter;
+        else           
+            sum -= ter;
+    }
+    return sum;
 }
