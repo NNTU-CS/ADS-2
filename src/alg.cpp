f9b0d90cff676acfe6ab16abfed9b8e750ab7977
Vondarr <vladislavradnov6@gmail.com>
2026-03-13 18:57:42 +0300

    Ряднов 25-ИСТ-1-2 alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,42 +4,42 @@
 
 
 double pown(double value, uint16_t n) {
-	if (n == 0)
-		return 1.0;
-	double result = 1;
-	for (uint16_t i = 0; i < n; i++)
-		result *= value;
-	return result;
+  if (n == 0)
+    return 1.0;
+  double result = 1;
+  for (uint16_t i = 0; i < n; i++)
+    result *= value;
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
-	uint64_t result = 1;
-	for (uint16_t i = 1; i <= n; i++)
-		result *= i;
-	return result;
+  uint64_t result = 1;
+  for (uint16_t i = 1; i <= n; i++)
+    result *= i;
+  return result;
 }
 
 double calcItem(double x, uint16_t n) {
-	return pown(x, n) / fact(n);
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-	double result = 1;
-	for (uint16_t i = 1; i <= count; i++)
-		result += calcItem(x, i);
-	return result;
+  double result = 1;
+  for (uint16_t i = 1; i <= count; i++)
+    result += calcItem(x, i);
+  return result;
 }
 
 double sinn(double x, uint16_t count) {
-	double result = 0;
-	for (uint16_t i = 1; i <= count; i++)
-		result += pown(-1, i-1) * calcItem(x, i * 2 - 1);
-	return result;
+  double result = 0;
+  for (uint16_t i = 1; i <= count; i++)
+    result += pown(-1, i-1) * calcItem(x, i * 2 - 1);
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
-	double result = 0;
-	for (uint16_t i = 1; i <= count; i++)
-		result += pown(-1, i - 1) * calcItem(x, i * 2 - 2);
-	return result;
+  double result = 0;
+  for (uint16_t i = 1; i <= count; i++)
+    result += pown(-1, i - 1) * calcItem(x, i * 2 - 2);
+  return result;
 }
