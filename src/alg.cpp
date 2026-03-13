495886df6b5f199b37caca0ec7f016225d116c48
EkaterinaShkolnikk <75201487+EkaterinaShkolnikk@users.noreply.github.com>
2026-03-13 09:14:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,46 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double resultat = 1;
+  for (uint16_t i = 1; i <= n; i++) 
+    resultat = resultat * value;
+  return resultat;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t resultat = 1;
+  for (uint16_t i = 1; i <= n; i++) 
+    resultat = resultat * i;
+  return resultat;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  double slogaemoeRiada = pown(x, n) / fact(n);
+  return slogaemoeRiada;
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double exponenta = 1;
+  for (uint16_t i = 1; i <= count; i++)
+    exponenta += calcItem(x, i);
+  return exponenta;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double sinus = x;
+  char znak = -1;
+  for (uint16_t i = 3; i <= count; i += 2) {
+    sinus = sinus + (znak * calcItem(x, i));
+    znak = znak * (-1);
+  }
+  return sinus;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double cosinus  = 1;
+  char znak = -1;
+  for (uint16_t i = 2; i <= count; i += 2) {
+    cosinus += znak * calcItem(x, i);
+  }
+  return cosinus;
 }
