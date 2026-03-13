3583fcf9ccbcc642ca0e764b9bafa061ace1c50c
TimDen1349 <tim.denisov.amur1@yandex.ru>
2026-03-13 16:41:53 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -34,16 +34,16 @@ double expn(double x, uint16_t count) {
 
 double sinn(double x, uint16_t count) {
     double res = x;
-    for (uint16_t i = 1; i < count; ++i) {
-        res += pown(-1, i - 1) * (pown(x, 2 * i - 1) / fact(2 * i - 1));
+    for (uint16_t n = 2; n <= count; ++n) {
+        res += pown(-1, n - 1) * (pown(x, 2 * n - 1) / fact(2 * n - 1));
     }
     return res;
 }
 
 double cosn(double x, uint16_t count) {
     double res = 1;
-    for (uint16_t i = 1; i < count; ++i) {
-        res += pown(-1, i - 1) * (pown(x, 2 * i - 2) / fact(2 * i - 2));
+    for (uint16_t n = 2; n <= count; ++n) {
+        res += pown(-1, n - 1) * (pown(x, 2 * n - 2) / fact(2 * n - 2));
     }
     return res;
 }
