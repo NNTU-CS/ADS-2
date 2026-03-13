a53f76e9c4ecea322bd22fb81798417dd928e85f
leon-234 <rust824@mail.ru>
2026-03-13 19:00:15 +0300

    Update alg.cpp
    Ильин. 25-ИСТ-1-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -20,40 +20,30 @@ uint64_t fact(uint16_t n) {
     return n * fact(n - 1);
 }
 
-double calcItem(double x, uint16_t n, char choice) {
-    switch (choice) { 
-    case 'e':
-        return pown(x, n) / fact(n);
-    case 's':
-        return pown(-1.0, n - 1) * (pown(x, (2 * n) - 1) / fact((2 * n) - 1));
-    case 'c':
-        return pown(-1.0, n - 1) * (pown(x, (2 * n) - 2) / fact((2 * n) - 2));
-    default:
-        cout << "Неверный выбор. Попробуйте снова" << endl;
-        return 0.0;  
-    }
+double calcItem(double x, uint16_t n) {
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-    double sum = 1.0;  
-    for (uint16_t i = 1; i < count; i++) {
-        sum += calcItem(x, i, 'e');
+    double sum = 0.0;
+    for (uint16_t i = 0; i < count; i++) {
+        sum += calcItem(x, i);
     }
     return sum;
 }
 
 double sinn(double x, uint16_t count) {
-    double sum = x;  
-    for (uint16_t i = 2; i < count; i++) {  
-        sum += calcItem(x, i, 's');
+    double sum = 0.0;
+    for (uint16_t i = 1; i < count; i++) {
+        sum += pown(-1.0, i - 1) * (pown(x, (2 * i) - 1) / fact((2 * i) - 1));
     }
     return sum;
 }
 
 double cosn(double x, uint16_t count) {
-    double sum = 1.0;  
-    for (uint16_t i = 1; i < count; i++) {  
-        sum += calcItem(x, i, 'c');
+    double sum = 0.0;
+    for (uint16_t i = 1; i < count; i++) { 
+        sum += pown(-1.0, i - 1) * (pown(x, (2 * i) - 2) / fact((2 * i) - 2));;
     }
     return sum;
 }
