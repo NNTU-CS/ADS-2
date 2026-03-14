74b8cc9d105e4cc5eebc2090e566c36a4e880874
peshinam <frrr.emaeeeee@mail.ru>
2026-03-15 00:57:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -20,8 +20,8 @@ double calcItem(double x, uint16_t n) {
 }
 
 double expn(double x, uint16_t count) {
-    double sum = 1.0;
-    for (int n = 1; n < count; n++) {
+    double sum = 0;
+    for(int n = 0; n <= count; n++) {
         sum += calcItem(x, n);
     }
     return sum;
