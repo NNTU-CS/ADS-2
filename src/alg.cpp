96f7e3e12661a51577f64c1bbdb38d18b3e91921
whoany <120975118+whoany@users.noreply.github.com>
2026-03-14 16:42:19 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,66 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
-#include "alg.h"
-
+#include <cstdint>
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+    double result = 1.0;
+    for (uint16_t i = 0; i < n; i++) {
+        result *= value;
+    }
+
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; i++) {
+        result = result * i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t n = 0; n < count; n++) {
+        result = result + calcItem(x, n);
+    }
+    return result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t n = 1; n <= count; n++) {
+        uint16_t power = 2 * n - 1;
+        double term = calcItem(x, power);
+        bool isNegative = (n % 2 == 0);
+
+        if (isNegative) {
+            result = result - term;
+        }
+        else {
+            result = result + term;
+        }
+    }
+    return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t n = 1; n <= count; n++) {
+        uint16_t power = 2 * n - 2;
+        double term = calcItem(x, power);
+        bool isNegative = (n % 2 == 0);
+
+        if (isNegative) {
+            result = result - term;
+        }
+        else {
+            result = result + term;
+        }
+    }
+    return result;
 }
