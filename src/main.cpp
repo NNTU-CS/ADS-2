9b97aa2b9bfa7e70c737851a1c01476dee4e9d90
krotolik2020-ctrl <Kro.tolik2020@gmail.com>
2026-03-14 18:44:05 +0300

    updated alg.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 // Copyright 2022 NNTU-CS
 #include "alg.h"
+#include <iostream>
 
 int main() {
-    return 0;
+  double val = 1.0;
+  uint16_t steps = 10;
+
+  std::cout << "Test Taylor for x = " << val << std::endl;
+  std::cout << "Exp: " << expn(val, steps) << std::endl;
+  std::cout << "Sin: " << sinn(val, steps) << std::endl;
+  std::cout << "Cos: " << cosn(val, steps) << std::endl;
+
+  return 0;
 }
