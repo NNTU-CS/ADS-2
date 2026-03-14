24c2be9a3d4c00f28d5e377d96cfa3526e381171
anton3798 <chervoniy.vladimir.152@gmail.com>
2026-03-14 22:11:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,67 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double k = value;
+    if (n == 0) {
+      return 1;
+    } else {
+      for (uint64_t i = 1; i < n; i++) {
+        k *= value;
+    }
+  }
+  return k;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t k = n;
+  uint64_t facti = 1;
+  if (n <= 1)
+    return 1;
+  else {
+    for (uint64_t i = 0; i < n; i++) {
+      facti *= k;
+      k -= 1;
+    }
+  }
+  return facti;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  double k = pown(x, n) / fact(n);
+  return k;
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double e = 1;
+  uint64_t n = 1;
+  for (uint64_t i = 0; i < count; i++) {
+    e += pown(x, n) / fact(n);
+    n += 1;
+  }
+  return e;
 }
-
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double e = 0;
+  uint64_t n = 1;
+  for (uint64_t i = 0; i < count; i++) {
+    if (i % 2 == 0)
+      e += pown(x, n) / fact(n);
+    else
+      e -= pown(x, n) / fact(n);
+    n += 2;
+  }
+  return e;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double e = 0;
+  uint64_t n = 0;
+  for (uint64_t i = 0; i < count; i++) {
+    if (i % 2 == 0)
+        e += pown(x, n) / fact(n);
+    else
+        e -= pown(x, n) / fact(n);
+    n += 2;
+  }
+  return e;
 }
