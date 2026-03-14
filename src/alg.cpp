f53307407604ee68876d3083f47440449a829076
peshinam <frrr.emaeeeee@mail.ru>
2026-03-15 00:39:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,13 +5,13 @@
 
 double pown(double value, uint16_t n) {
     double res = 1;
-    for(int i = 0; i < n; i++) res *= value;
+    for (int i = 0; i < n; i++) res *= value;
     return res;
 }
 
 uint64_t fact(uint16_t n) {
     int res = 1;
-    for(int i = 2; i <= n; i++) res *= i;
+    for (int i = 2; i <= n; i++) res *= i;
     return res;
 }
 
@@ -21,15 +21,15 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
     double sum = 0;
-    for(int n = 0; n < count; n++) sum += calcItem(x, n);
+    for (int n = 0; n < count; n++) sum += calcItem(x, n);
     return sum;
 }
 
 double sinn(double x, uint16_t count) {
     double sum = 0;
-    for(int n = 1; n <= count; n++) {
+    for (int n = 1; n <= count; n++) {
         double term = calcItem(x, 2*n - 1);
-        if(n % 2 == 1) sum += term;
+        if (n % 2 == 1) sum += term;
         else sum -= term;
     }
     return sum;
@@ -37,9 +37,9 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
     double sum = 1;
-    for(int n = 2; n <= count; n++) {
+    for (int n = 2; n <= count; n++) {
         double term = calcItem(x, 2*n - 2);
-        if(n % 2 == 1) sum += term;
+        if (n % 2 == 1) sum += term;
         else sum -= term;
     }
     return sum;
