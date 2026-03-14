ff1d2ad55e5fb3a36d9db2038e0d9278b1f27c57
vladsundukov1-bit <vladsundukov1@gmail.com>
2026-03-14 17:15:57 +0300

    Сюндюков Владислав 25-ист-3

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,57 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+    double rezult = 1;
+    for (uint16_t i = 0; i < n; i++) {
+      rezult *= value;
+  }
+  return rezult;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t rezult = 1;
+  for (uint16_t i = 1; i <= n; i++) {
+      rezult *= i;
+    }
+    return rezult;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double sumc = 0;
+  for (uint16_t i = 0; i <= count; i++) {
+      sumc += calcItem(x, i);
+    }
+    return sumc;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double sumc = 0;
+    for (uint16_t i = 0; i < count; i++) {
+        uint16_t num = 2 * i + 1;
+        double s = pown(x, num) / fact(num);
+        if (i % 2 == 0) {
+            sumc += s;
+        } else {
+            sumc -= s;
+        }
+    }
+    return sumc;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double sumc = 0;
+  for (uint16_t i = 0; i < count; i++) {
+      uint16_t num = 2 * i;
+      double c = pown(x, num) / fact(num);
+        if (i % 2 == 0) {
+            sumc += c;
+        } else {
+            sumc -= c;
+        }
+    }
+    return sumc;
 }
