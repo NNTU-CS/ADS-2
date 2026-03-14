3294bdd1c4c0938b713cb08d46b6d6f047b0b274
PoP1pu <101626112+PoP1pu@users.noreply.github.com>
2026-03-14 18:35:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,53 +11,49 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-	uint64_t res = 1;
-	for (uint16_t i = 2; i <= n; i++) {
-		res *= i;
-	}
-	return res;
+    uint64_t res = 1;
+    for (uint16_t i = 2; i <= n; i++) {
+        res *= i;
+    }
+    return res;
 }
 
 double calcItem(double x, uint16_t n) {
-	return pown(x, n) / fact(n);
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-	double res = 0.0;
-	for (uint16_t n = 0; n <= count; n++) {
-		res += calcItem(x, n);
-	}
-	return res;
+    double res = 0.0;
+    for (uint16_t n = 0; n <= count; n++) {
+        res += calcItem(x, n);
+    }
+    return res;
 }
 
 double sinn(double x, uint16_t count) {
-	double res = 0.0;
-	for (uint16_t n = 1; n <= count; n++) {
-		uint16_t power = 2 * n - 1;
-		double term = calcItem(x, power);
-		bool isNegative = (n % 2 == 0);
-		if (isNegative) {
-			res -= term;
-		} 
-		else {
-			res += term;
-		}
-	}
-	return res;
+    double res = 0.0;
+    for (uint16_t n = 1; n <= count; n++) {
+        uint16_t power = 2 * n - 1;
+        double term = calcItem(x, power);
+        bool isNegative = (n % 2 == 0);
+        if (isNegative)
+            res -= term;
+        else
+            res += term;
+    }
+    return res;
 }
 
 double cosn(double x, uint16_t count) {
-	double res = 0.0;
-	for (uint16_t n = 1; n <= count; n++) {
-		uint16_t power = 2 * n - 2;
-		double term = calcItem(x, power);
-		bool isNegative = (n % 2 == 0);
-		if (isNegative) {
-			res -= term;
-		} 
-		else {
-			res += term;
-		}
-	}
-	return res;
+    double res = 0.0;
+    for (uint16_t n = 1; n <= count; n++) {
+        uint16_t power = 2 * n - 2;
+        double term = calcItem(x, power);
+        bool isNegative = (n % 2 == 0);
+        if (isNegative)
+            res -= term;
+        else
+            res += term;
+    }
+    return res;
 }
