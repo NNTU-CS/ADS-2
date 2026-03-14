37e0274bb3f02ced80aff694b371f870ab9e7377
Anton <antonfrolov1977@gmail.com>
2026-03-14 22:18:09 +0300

    Update 14.03 alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,51 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double ans = 1.0;
+  for (uint16_t i = 0; i < n; ++i) {
+    ans *= value;
+  }
+  return ans;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t res = 1;
+  for (uint16_t i = 2; i <= n; ++i) {
+    res *= i;
+  }
+  return res;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / (double)fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    sum += calcItem(x, n);
+  }
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    uint16_t k = 2 * n + 1;
+    double term = pown(x, k) / (double)fact(k);
+    if (n % 2) term = -term;
+    sum += term;
+  }
+  return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    uint16_t k = 2 * n;
+    double term = pown(x, k) / (double)fact(k);
+    if (n % 2) term = -term;
+    sum += term;
+  }
+  return sum;
 }
