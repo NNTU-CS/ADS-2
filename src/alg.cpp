b4107c0efa4cbcf1abaf5f52dc73fbaf9512a8a1
gllyforg <shilovaksenia2018@mail.ru>
2026-03-14 21:57:00 +0300

    Update alg.cpp
    Шилова 25-ИСТ-1-1

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,55 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double result = 1.0;
+  for (uint16_t i = 0; i < n; ++i) {
+      result = result * value;
+  }
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t result = 1;
+  for (uint16_t i = 2; i <= n; ++i) {
+      result = result * i;
+  }
+  return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+      result = result + calcItem(x, n);
+  }
+  return result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+      double sign = 1.0;
+      if (n % 2 == 1) {
+          sign = -1.0;
+      }
+      uint16_t power = 2 * n + 1;
+      result = result + sign * pown(x, power) / fact(power);
+  }
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+      double sign = 1.0;
+      if (n % 2 == 1) {
+            sign = -1.0;
+      }
+      uint16_t power = 2 * n;
+      result = result + sign * pown(x, power) / fact(power);
+  }
+  return result;
 }
