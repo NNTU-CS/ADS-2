7b33008c2d52ead1bfb76738d28b3c093a7d297e
errfct <veronika.sokolova.07@mail.ru>
2026-03-14 17:32:03 +0300

    Code-1.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,59 @@
 // Copyright 2022 NNTU-CS
-#include <cstdint>
 #include "alg.h"
-
+#include <cstdint>
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+    if (n == 0) return 1.0;
+    double result = 1.0;
+    for (uint16_t i = 0; i < n; ++i) {
+        result *= value;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    if (n == 0 || n == 1) return 1;
+    uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t n = 0; n < count; ++n) {
+        result += calcItem(x, n);
+    }
+    return result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t n = 0; n < count; ++n) {
+        uint16_t power = 2 * n + 1;
+        double item = calcItem(x, power);
+        if (n % 2 == 1) {
+            item *= -1;
+        }
+        result += item;
+    }
+    return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double result = 0.0;
+    for (uint16_t n = 0; n < count; ++n) {
+        uint16_t power = 2 * n;
+        double item = calcItem(x, power);
+        if (n % 2 == 1) {
+            item *= -1;
+        }
+        result += item;
+    }
+    return result;
 }
