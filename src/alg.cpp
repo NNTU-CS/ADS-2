e280e44ef5dcaead04896fe19bcd2255a995ee79
Landysh <walfa45milana@mail.ru>
2026-03-14 22:04:52 +0300

    Chjen-25IST41.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,12 +1,9 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
-#include <iostream>
-#include <cmath>
-
 
 double pown(double value, uint16_t n) {
-    double Ires = 1;
+    double Ires = 1.0;
     for (int i = 0; i < n; ++i) {
         Ires *= value;
     }
@@ -14,48 +11,53 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-    uint64_t ansfact = 1;
-    for (int i = 1; i <= n; ++i) {
-        ansfact *= i;
+    if (n == 0) return 1;
+    uint64_t Tres = 1;
+    for (uint16_t i = 1; i <= n; ++i) {
+        Tres *= i;
     }
-    return ansfact;
+    return Tres;
 }
 
 double calcItem(double x, uint16_t n) {
-        return pown(x, n) / fact(n);
+    if (n == 0) return 1.0;
+    double Cres = 1.0;
+    for (uint16_t i = 1; i <= n; ++i) {
+        Cres *= x / i;
+    }
+    return Cres;
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
-    double sum = 0.0;
-    for (uint16_t n = 0; n <= count; ++n) {
-        sum += calcItem(x, n);
+    if (count == 0) return 0.0;
+    double resi = 1.0;
+    double temp = 1.0;
+    for (uint16_t n = 1; n <= count; n++) {
+        temp *= x / n;
+        resi += temp;
     }
-    return sum;
+    return resi;
 }
 
 double sinn(double x, uint16_t count) {
-        double sum = 0.0;
-    for (uint16_t n = 1; n <= count; ++n) {
-        double term = pown(x, 2*n - 1) / fact(2*n - 1);
-        if (n % 2 == 0) {
-            sum -= term;
-        } else {
-            sum += term;
-        }
+    if (count == 0) return 0.0;
+    double Res = 0.0;
+    double temp = x;
+    Res += temp;
+    for (uint16_t n = 1; n < count; ++n) {
+        temp *= -x * x / (2 * n * (2 * n + 1));
+        Res += temp;
     }
-    return sum;
+    return Res;
 }
 
 double cosn(double x, uint16_t count) {
-        double sum = 0.0;
-    for (uint16_t n = 1; n <= count; ++n) {
-        double term = pown(x, 2*n - 2) / fact(2*n - 2);
-        if (n % 2 == 0) {
-            sum -= term;
-        } else {
-            sum += term;
-        }
+    if (count == 0) return 0.0;
+    double rest = 1.0;
+    double temp = 1.0;
+    for (uint16_t n = 1; n < count; ++n) {
+        temp *= -x * x / ((2 * n - 1) * (2 * n));
+        rest += temp;
     }
-    return sum;
+    return rest;
 }
