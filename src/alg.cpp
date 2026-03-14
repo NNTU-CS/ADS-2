91b790042d2c1503897c17a6209710ed5df35df6
peshinam <frrr.emaeeeee@mail.ru>
2026-03-15 00:26:13 +0300

    ads2 alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,43 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+    double res = 1;
+    for(int i = 0; i < n; i++) res *= value;
+    return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    int res = 1;
+    for(int i = 2; i <= n; i++) res *= i;
+    return res;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 0;
+    for(int n = 0; n < count; n++) sum += calcItem(x, n);
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 0;
+    for(int n = 1; n <= count; n++) {
+        double term = calcItem(x, 2*n - 1);
+        if(n % 2 == 1) sum += term;
+        else sum -= term;
+    }
+    return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 1;
+    for(int n = 2; n <= count; n++) {
+        double term = calcItem(x, 2*n - 2);
+        if(n % 2 == 1) sum += term;
+        else sum -= term;
+    }
+    return sum;
 }
