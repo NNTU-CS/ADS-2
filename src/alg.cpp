990f2149469a58b6efbac68f03da28453d8c43f5
AlinaShvetsova16 <alinashvetsova16@gmail.com>
2026-03-14 21:01:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,7 +19,7 @@ uint64_t fact(uint16_t n) {
 
 double calcItem(double x, uint16_t n) {
   double result = 1.0;
-  for (uint16_t i = 1; i <= n; i++){
+  for (uint16_t i = 1; i <= n; i++) {
     result *= x / i;
   }
   return result;
@@ -28,7 +28,7 @@ double calcItem(double x, uint16_t n) {
 double expn(double x, uint16_t count) {
   double result = 1.0;
   double date = 1.0;
-  for (uint16_t n = 1; n <= count; n++){
+  for (uint16_t n = 1; n <= count; n++) {
     date *= x / n;
     result += date;
   }
@@ -38,7 +38,7 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
   double result = 0.0;
   double date = x;
-  for (uint16_t n = 1; n <= count; n++){
+  for (uint16_t n = 1; n <= count; n++) {
     if (n % 2 == 1) {
       result += date;
     } else {
@@ -52,7 +52,7 @@ double sinn(double x, uint16_t count) {
 double cosn(double x, uint16_t count) {
   double result = 1.0;
   double date = 1.0;
-  for (uint16_t n = 1; n <= count; n++){
+  for (uint16_t n = 1; n <= count; n++) {
     if (n % 2 == 1) {
       result -= date;
     } else {
