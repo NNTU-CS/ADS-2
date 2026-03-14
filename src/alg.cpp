fd414c8ad02834bcdabe313b42936ec4db03fbe9
amnesssiaaa <zvezda15032007@gmail.com>
2026-03-14 20:18:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,50 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double result = 1.0;
+  for (uint16_t i = 0; i < n; i++) {
+    result *= value;
+  }
+  return result; 
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n == 1) 
+    return 1;
+  else
+    return n*fact(n-1);
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x,n) / fact (n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0;
+  for (uint16_t i = 0; i <= count; i++) {
+    	sum += calcItem(x, i);
+  }
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0;
+  for (uint16_t i = 1; i <= count; i++) {
+      double pown1 = pown((-1),(i-1));
+      double pown2 = pown(x,(2*i-1));
+      uint64_t fact1 = fact(2*i-1);
+      sum += pown1 * (pown2 / fact1);
+  }
+  return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double sum = 0;
+  for (uint16_t i = 1; i <= count; i++) {
+      double pown1 = pown((-1),(i-1));
+      double pown2 = pown(x,(2*i-2));
+      uint64_t fact1 = fact(2*i-2);
+      sum += pown1 * (pown2 / fact1);
+  	}
+  return sum;
 }
