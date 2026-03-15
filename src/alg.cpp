ef5eeebe769124fb5ba9e268786cb2f9f51027ae
SonyaShem <sonyasha2101@gmail.com>
2026-03-15 21:59:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -48,7 +48,7 @@ double cosn(double x, uint16_t count) {
   double Result = 0;
   double tmp = 1;
 
-  for (uint16_t i = 0; i <= count; i++) {
+  for (uint16_t i = 1; i <= count; i++) {
     Result += tmp;
     tmp *= -x * x / ((2 * i + 1) * (2 * i + 2));
   }
