dca21e4095220b50c0ee4f741d514fe3e5c7b00a
polinanaumovaa <pnaumova32@gmail.com>
2026-03-15 19:00:34 +0300

    Наумова Полина 25-ИСТ-3.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,55 +4,55 @@
 
 
 double pown(double value, uint16_t n) {
-  double result = 1.0;
-  for(uint16_t i = 0; i < n; ++i) {
-    result *= value;
-  }
-  return result;
+    double result = 1.0;
+    for (uint16_t i = 0; i < n; ++i) {
+        result *= value;
+    }
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  uint64_t result = 1;
-  for (uint16_t i = 2; i <= n; ++i) {
-    result *= i;
-  }
-  return result;
+    uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return pown(x, n) / static_cast<double>(fact(n));
+    return pown(x, n) / static_cast<double>(fact(n));
 }
 
 double expn(double x, uint16_t count) {
-  double result = 0.0;
-  for (uint16_t n = 0; n < count; ++n) {
-    result += calcItem(x, n);
-  }
-  return result;
+    double result = 0.0;
+    for (uint16_t n = 0; n < count; ++n) {
+        result += calcItem(x, n);
+    }
+    return result;
 }
 
 double sinn(double x, uint16_t count) {
-  double result = 0.0;
-  for (uint16_t n = 0; n < count; ++n) {
-    double term = pown(x, 2 * n + 1) / static_cast<double>(fact(2 * n + 1));
-    if (n % 2 == 0) {
-      result += term;
-    } else {
-      result -= term;
+    double result = 0.0;
+    for (uint16_t n = 0; n < count; ++n) {
+        double term = pown(x, 2 * n + 1) / static_cast<double>(fact(2 * n + 1));
+        if (n % 2 == 0) {
+            result += term;
+        } else {
+            result -= term;
+        }
     }
-  }
-  return result;
+    return result;
 }
 
 double cosn(double x, uint16_t count) {
-  double result = 0.0;
-  for (uint16_t n = 0; n < count; ++n) {
-    double term = pown(x, 2 * n) / static_cast<double>(fact(2 * n));
-    if (n % 2 == 0) {
-      result += term;
-    } else {
-      result -= term;
+    double result = 0.0;
+    for (uint16_t n = 0; n < count; ++n) {
+        double term = pown(x, 2 * n) / static_cast<double>(fact(2 * n));
+        if (n % 2 == 0) {
+            result += term;
+        } else {
+            result -= term;
+        }
     }
-  }
-  return result;
+    return result;
 }
