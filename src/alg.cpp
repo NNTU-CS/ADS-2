068e93e949050556e20bed7c6d95ac8bccec6284
amnesssiaaa <zvezda15032007@gmail.com>
2026-03-15 13:25:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,24 +8,24 @@ double pown(double value, uint16_t n) {
   for (uint16_t i = 0; i < n; i++) {
     result *= value;
   }
-  return result; 
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
-  if (n == 1) 
+  if (n == 1)
     return 1;
   else
     return n*fact(n-1);
 }
 
 double calcItem(double x, uint16_t n) {
-  return pown(x,n) / fact (n);
+  return pown(x, n) / fact (n);
 }
 
 double expn(double x, uint16_t count) {
   double sum = 0;
   for (uint16_t i = 0; i <= count; i++) {
-    	sum += calcItem(x, i);
+    sum += calcItem(x, i);
   }
   return sum;
 }
@@ -33,8 +33,8 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
   double sum = 0;
   for (uint16_t i = 1; i <= count; i++) {
-      double pown1 = pown((-1),(i-1));
-      double pown2 = pown(x,(2*i-1));
+      double pown1 = pown((-1), (i-1));
+      double pown2 = pown(x, (2*i-1));
       uint64_t fact1 = fact(2*i-1);
       sum += pown1 * (pown2 / fact1);
   }
@@ -44,10 +44,10 @@ double sinn(double x, uint16_t count) {
 double cosn(double x, uint16_t count) {
   double summ = 0;
   for (uint16_t i = 1; i <= count; i++) {
-      double pown1 = pown((-1),(i-1));
-      double pown2 = pown(x,(2*i-2));
+      double pown1 = pown((-1), (i-1));
+      double pown2 = pown(x, (2*i-2));
       uint64_t fact1 = fact(2*i-2);
       summ += pown1 * (pown2 / fact1);
-  	}
+  }
   return summ;
 }
