9908052b46c330b126a74ab0ee8b591f92113c96
pabold1337 <apa2007nn@gmail.com>
2026-03-15 19:43:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,7 +12,8 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-    if (n == 0 || n == 1) return 1;
+    if (n == 0 || n == 1)
+        return 1;
 
     uint64_t result = 1;
     for (uint16_t i = 2; i <= n; i++) {
@@ -53,7 +54,6 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
     double sum = 1.0;
-
     for (uint16_t n = 1; n < count; n++) {
         uint16_t exponent = 2 * n;
         double term = pown(x, exponent) / fact(exponent);
