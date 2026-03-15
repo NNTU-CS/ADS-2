1f296452b67df78989a6793c1f7fef6d1e2a5115
Molman0 <evgenytyukalkin@yandex.com>
2026-03-15 17:51:50 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,53 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double res = 1;
+  if (n == 0)
+    return res;
+  else
+    for (int i = 0; i < n; i++)
+      res *= value;
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n == 0)
+    return 1;
+  uint64_t factorial = 1;
+  for (uint32_t i = 1; i <= n; i++) {
+    factorial *= i;
+  }
+  return factorial;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double exp = 0;
+  for (int i = 0; i < count; i++) {
+    exp += calcItem(x, count);
+  }
+  return exp;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  uint16_t j = 1;
+  double res = 0;
+  for (uint16_t i = 1; i < count; i++) {
+    res += pown(-1, i - 1) * pown(x, j) / fact(j);
+    j += 2;
+  }
+  return res;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  uint16_t j = 0;
+  double res = 0;
+  for (uint16_t i = 1; i < count; i++) {
+    res += pown(-1, i - 1) * pown(x, j) / fact(j);
+    j += 2;
+  }
+  return res;
 }
