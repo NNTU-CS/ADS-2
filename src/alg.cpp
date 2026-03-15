0628d91b8c7eb152217b1b3498a60e214f29eab1
Artem-Bld <baluevartem07@mail.ru>
2026-03-15 21:26:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,57 +4,57 @@
 
 
 double pown(double value, uint16_t n) {
-	double res = 1.0;
-	for (uint16_t i = 0; i < n; i++) {
-		res *= value;
-	}
-	return res;
+  double res = 1.0;
+  for (uint16_t i = 0; i < n; i++) {
+    res *= value;
+  }
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-	uint64_t result = 1;
-	for (uint16_t i = 2; i <= n; i++) {
-		result *= i;
-	}
-	return result;
+  uint64_t result = 1;
+  for (uint16_t i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 }
 
 double calcItem(double x, uint16_t n) {
-	return pown(x,n) / static_cast<double>(fact(n));
+  return pown(x, n) / static_cast<double>(fact(n));
 }
 
 double expn(double x, uint16_t count) {
-	double sum = 1.0;
-	for (uint16_t n = 1; n <= count; n++) {
-		sum += calcItem(x, n);
-	}
-	return sum;
+  double sum = 1.0;
+  for (uint16_t n = 1; n <= count; n++) {
+    sum += calcItem(x, n);
+  }
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
-	double sum = 0.0;
-	for (uint16_t k = 1; k <= count; k++) {
-		uint16_t n = 2 * k - 1; 
-		double item = pown(x, n) / static_cast<double>(fact(n));
-		if ((k - 1) % 2 == 0) {
-			sum += item;
-		} else {
-			sum -= item;
-		}
-	}
-	return sum;
+  double sum = 0.0;
+  for (uint16_t k = 1; k <= count; k++) {
+    uint16_t n = 2 * k - 1;
+    double item = pown(x, n) / static_cast<double>(fact(n));
+    if ((k - 1) % 2 == 0) {
+      sum += item;
+    } else {
+      sum -= item;
+    }
+  }
+  return sum;
 }
 
 double cosn(double x, uint16_t count) {
-	double sum = 0.0;
-	for (uint16_t k = 1; k <= count; k++) {
-		uint16_t n = 2 * k - 2; 
-		double item = pown(x, n) / static_cast<double>(fact(n));
-		if ((k - 1) % 2 == 0) {
-			sum += item;
-		} else {
-			sum -= item;
-		}
-	}
-	return sum;
+  double sum = 0.0;
+  for (uint16_t k = 1; k <= count; k++) {
+    uint16_t n = 2 * k - 2;
+    double item = pown(x, n) / static_cast<double>(fact(n));
+    if ((k - 1) % 2 == 0) {
+      sum += item;
+    } else {
+      sum -= item;
+    }
+  }
+  return sum;
 }
