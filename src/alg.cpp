a803d0d972baedfc7285591e6cc690ab86b02a8b
IlyaZabivnoyy123 <ilyameiplay@gmail.com>
2026-03-15 22:53:18 +0300

    Доможиров Илья ИСТ-3.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,24 +5,61 @@
 
 double pown(double value, uint16_t n) {
   return 0.0;
+  double res = 1.0;
+  for (uint16_t i = 0; i < n; i++) {
+    res *= value;
+  }
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
   return 0.0;
+  uint64_t res = 1;
+  for (uint16_t i = 1; i <= n; i++) {
+    res *= i;
+  }
+  return res;
 }
 
 double calcItem(double x, uint16_t n) {
   return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
   return 0.0;
+  double sum = 1.0;
+  double term = 1.0;
+
+  for (uint16_t n = 1; n <= count; n++) {
+    term *= x / n;
+    sum += term;
+  }
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
   return 0.0;
+  double sum = 0.0;
+  for (uint16_t n = 0; n < count; n++) {
+    uint16_t k = 2 * n + 1;
+    double term = pown(x, k) / fact(k);
+    if (n % 2 == 1)
+      term = -term;
+    sum += term;
+  }
+  return sum;
 }
 
 double cosn(double x, uint16_t count) {
   return 0.0;
+  double sum = 0.0;
+  for (uint16_t n = 0; n < count; n++) {
+    uint16_t k = 2 * n;
+    double term = pown(x, k) / fact(k);
+    if (n % 2 == 1)
+      term = -term;
+    sum += term;
+  }
+  return sum;
 }
