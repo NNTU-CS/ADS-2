d498139597ab03143e645c196a8e2b3188d408f6
krotolik2020-ctrl <Kro.tolik2020@gmail.com>
2026-03-15 14:26:25 +0300

    updated alg.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,9 @@
 int main() {
   double val = 1.0;
   uint16_t steps = 10;
-
   std::cout << "Test Taylor for x = " << val << std::endl;
   std::cout << "Exp: " << expn(val, steps) << std::endl;
   std::cout << "Sin: " << sinn(val, steps) << std::endl;
   std::cout << "Cos: " << cosn(val, steps) << std::endl;
-
   return 0;
 }
