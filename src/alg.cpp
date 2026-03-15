ff970b3395d2e0d153460eb73ae42485b88ddf33
Rost870 <60008611+Rost870@users.noreply.github.com>
2026-03-15 16:38:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,35 +4,39 @@
 
 
 double pown(double value, uint16_t n) {
-  double a=1;
-  for(u_int16_t i=0;i<n;i++){
-        a*=value;
+    double a = 1;
+    for(uint16_t i = 0; i < n; i++) {
+        a *= value;
     }
     return a;
 }
 
 uint64_t fact(uint16_t n) {
-  if (n <= 1) return 1;
-  return n * fact(n - 1);
+    uint64_t a = 1;
+    for(uint16_t i = 1; i <= n; i++) {
+        a *= i;
+    }
+    return a;
 }
 
 double calcItem(double x, uint16_t n) {
-  return pown(x,n)/fact(n);
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  double sum=0;
-  for(uint16_t i=0;i<count;i++){
-    sum+=calcItem(x,i);
-  }
-  return sum;
+    double sum = 0;
+    for(uint16_t i = 0; i < count; i++) {
+        sum += calcItem(x, i);
+    }
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  double sum=0;
-   for(uint16_t i = 1; i <= count; i++) {  
-        uint16_t exp = 2*i - 1;        
+    double sum = 0;
+    for(uint16_t i = 1; i <= count; i++) {  
+        uint16_t exp = 2 * i - 1;        
         double ter = pown(x, exp) / fact(exp);
+        
         if(i % 2 == 1)  
             sum += ter;
         else            
@@ -43,9 +47,10 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
     double sum = 0;
-    for(uint16_t i= 0; i < count; i++) {  
-        uint16_t exp = 2*i;          
+    for(uint16_t i = 0; i < count; i++) {  
+        uint16_t exp = 2 * i;          
         double ter = pown(x, exp) / fact(exp);
+        
         if(i % 2 == 0)  
             sum += ter;
         else           
