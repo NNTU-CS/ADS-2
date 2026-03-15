f478be97a136817ab849df0654c90dc9c4f26ac3
PLAVLENNbIY-CbIPO4EK <ilya.potor2713@gmail.com>
2026-03-15 19:22:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,59 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  if (n == 0) return 1.0;
+  double result = 1.0;
+  for (uint16_t i = 0; i < n; ++i) {
+      result *= value;
+  }
+  return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  uint64_t result = 1;
+  for (uint16_t i = 2; i <= n; ++i) {
+    result *= i;
+  }
+  return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  double stp = pown(x, n);
+  uint64_t fct = fact(n);
+  return stp / fct;
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+      result += calcItem(x, n);
+  }
+  return result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    uint16_t p = 2*n + 1;
+    double t = calcItem(x, p);
+    if (n % 2)
+      t = -t;
+    result += t;
+  }
+  return result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double result = 0.0;
+  for (uint16_t n = 0; n < count; ++n) {
+    uint16_t p = 2*n;
+    double t = calcItem(x, p);
+    if (n % 2)
+      t = -t;
+    result += t;
+  }
+  return result;
 }
+
+
+
