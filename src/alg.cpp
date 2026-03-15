7149e364086eb160e12eff5e4ab4f2dd41951adc
PRYANIK48 <112880306+PRYANIK48@users.noreply.github.com>
2026-03-15 23:24:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,8 +6,7 @@
 double pown(double value, uint16_t n) {
     if (n == 0) {
         return 1;
-    }
-    else {
+    } else {
         double multiplier = value;
         for (uint64_t i = 1; i < n; i++) {
             value *= multiplier;
