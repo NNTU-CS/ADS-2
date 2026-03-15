52caef699c037c725a8b5bd5e3b9deeff447249c
Rost870 <60008611+Rost870@users.noreply.github.com>
2026-03-15 17:33:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,58 +3,61 @@
 #include "alg.h"
 
 
+// Copyright 2022 NNTU-CS
+#include "alg.h"
+#include <cstdint>
+
 double pown(double value, uint16_t n) {
-    double a = 1;
-    for(uint16_t i = 0; i < n; i++) {
-        a *= value;
-    }
-    return a;
+  double a = 1;
+  for (uint16_t i = 0; i < n; i++) {
+    a *= value;
+  }
+  return a;
 }
 
 uint64_t fact(uint16_t n) {
-    uint64_t a = 1;
-    for(uint16_t i = 1; i <= n; i++) {
-        a *= i;
-    }
-    return a;
+  uint64_t a = 1;
+  for (uint16_t i = 1; i <= n; i++) {
+    a *= i;
+  }
+  return a;
 }
 
-double calcItem(double x, uint16_t n) {
-    return pown(x, n) / fact(n);
-}
+double calcItem(double x, uint16_t n) { return pown(x, n) / fact(n); }
 
 double expn(double x, uint16_t count) {
-    double sum = 0;
-    for(uint16_t i = 0; i <= count; i++) {
-        sum += calcItem(x, i);
-    }
-    return sum;
+  double sum = 0;
+  for (uint16_t i = 0; i <= count; i++) {
+    sum += calcItem(x, i);
+  }
+  return sum;
 }
 
 double sinn(double x, uint16_t count) {
-    double sum = 0;
-    for(uint16_t i = 1; i <= count; i++) {  
-        uint16_t exp = 2 * i - 1;        
-        double ter = pown(x, exp) / fact(exp);
-        
-        if(i % 2 == 1)  
-            sum += ter;
-        else            
-            sum -= ter;
-    }
-    return sum;
+  double sum = 0;
+  for (uint16_t i = 1; i <= count; i++) {
+    uint16_t exp = 2 * i - 1;
+    double ter = pown(x, exp) / fact(exp);
+
+    if (i % 2 == 1)
+      sum += ter;
+    else
+      sum -= ter;
+  }
+  return sum;
 }
 
 double cosn(double x, uint16_t count) {
-    double sum = 0;
-    for(uint16_t i = 0; i < count; i++) {  
-        uint16_t exp = 2 * i;          
-        double ter = pown(x, exp) / fact(exp);
-        
-        if(i % 2 == 0)  
-            sum += ter;
-        else           
-            sum -= ter;
-    }
-    return sum;
+  double sum = 0;
+  for (uint16_t i = 0; i < count; i++) {
+    uint16_t exp = 2 * i;
+    double ter = pown(x, exp) / fact(exp);
+
+    if (i % 2 == 0)
+      sum += ter;
+    else
+      sum -= ter;
+  }
+  return sum;
 }
+
