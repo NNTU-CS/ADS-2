c4e8b5f087bf1775f1d0b2b3663b9f48d21ac762
SonyaShem <sonyasha2101@gmail.com>
2026-03-15 15:37:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,45 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  long double OVal = 1;
+  for (uint16_t i = 0; i < n; i++) {
+    OVal *= value;
+  }
+  return OVal;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  unsigned long long Result = 1;
+  for (uint16_t i = 1; i <= n; i++) {
+    Result *= i;
+  }
+  return Result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double Result = 0;
+  for (uint16_t i = 0; i < count; i++){
+    Result += pown(x, i) / fact(count);
+  }
+  return Result;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double Result = 0;
+  for (uint16_t i = 0; i < count; i++){
+    Result += pown(-1, count - 1) * (pown(x, 2 * count - 1) / fact(2 * count - 1));
+  }
+  return Result;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double Result = 0;
+  for (uint16_t i = 0; i < count; i++){
+    Result += pown(-1, count - 1) * (pown(x, 2 * count - 2) / fact(2 * count - 2));
+  }
+  return Result;
 }
