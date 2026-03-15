6c51c2a26ec5cad346a84415347583bad22aa12d
sw3rq <layza2k13@gmail.com>
2026-03-15 19:42:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,28 +1,76 @@
 // Copyright 2022 NNTU-CS
+
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-  return 0.0;
+    double result = 1.0;
+
+    for (uint16_t i = 0; i < n; i++) {
+        result = result * value;
+    }
+
+    return result;
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    uint64_t result = 1;
+
+    for (uint16_t i = 1; i <= n; i++) {
+        result = result * i;
+    }
+
+    return result;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    double power = pown(x, n);
+    uint64_t factorial = fact(n);
+
+    return power / factorial;
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 0.0;
+
+    for (uint16_t n = 0; n <= count; n++) {
+        double term = calcItem(x, n);
+        sum = sum + term;
+    }
+
+    return sum;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 0.0;
+
+    for (uint16_t n = 0; n < count; n++) {
+
+        double sign = pown(-1.0, n);
+        double power = pown(x, 2 * n + 1);
+        uint64_t factorial = fact(2 * n + 1);
+
+        double term = sign * power / factorial;
+
+        sum = sum + term;
+    }
+
+    return sum;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    double sum = 0.0;
+
+    for (uint16_t n = 0; n < count; n++) {
+
+        double sign = pown(-1.0, n);
+        double power = pown(x, 2 * n);
+        uint64_t factorial = fact(2 * n);
+
+        double term = sign * power / factorial;
+
+        sum = sum + term;
+    }
+
+    return sum;
 }
