eb67f8c56835c5e8ab678e6a0e3557d3fa3a2fe3
kazasaki23311 <julia.nikashina@gmail.com>
2026-03-15 17:29:25 +0300

    ads-2 nikashina.cpp
    Никашина Юлия 1-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,27 +2,37 @@
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-  return 0.0;
+  if (n == 0) return 1;
+  else {
+    double x = value;
+    for (uint16_t i = 1; i < n; i++) {
+      x *= value;
+    }
+    return x;
+  }
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n <= 1) return 1;
+  else return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  if (count == 0) return 1;
+  else return calcItem(x, count) + expn(x, count - 1);
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  if (count == 1) return x;
+  else return pown(-1, count - 1) * calcItem(x, 2 * count - 1) + sinn(x, count - 1);
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  if (count == 1) return 1;
+  else return pown(-1, count - 1) * calcItem(x, 2 * count - 2) + cosn(x, count - 1);
 }
