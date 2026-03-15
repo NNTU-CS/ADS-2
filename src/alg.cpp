a410a0e50a6bbc05db73b1a91df6f15b955a9353
kv1nk <kvink.ggs@gmail.com>
2026-03-15 23:33:02 +0300

    First commit

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,27 +2,45 @@
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-  return 0.0;
+    if (n == 0) {
+      return 1;
+    } else {
+      return value * pown(value, n - 1);
+    }
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+    if (n == 0) {
+      return 1;
+    } else {
+      return n * fact(n - 1);
+    }
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+    if (count == 0) {
+      return 1;
+    }
+    return calcItem(x, count) + expn(x, count - 1);
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+    if (count == 0) {
+      return 0;
+    }
+    return pown(-1, count - 1) * calcItem(x, 2 * count - 1) +
+    sinn(x, count - 1);
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+    if (count == 0) {
+      return 0;
+    }
+    return pown(-1, count - 1) * calcItem(x, 2 * count - 2) +
+    cosn(x, count - 1);
 }
