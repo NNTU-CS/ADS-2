f224446b48166b156e1c35807dc60bf9999db889
neywks <netarly0102200@gmail.com>
2026-03-15 16:36:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,51 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  double res = 1.0;
+  for (uint16_t i = 0; i < n; i++) {
+    res *= value;
+  }
+  return res;
 }
 
+
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n == 1)
+    return 1;
+  else
+    return n*fact(n-1)
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return pown(x,n) / fact(n)
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double res = 0.0;
+  for (uint16_t i = 0; i <= count; i++) {
+    res += calcItem(x, i);
+  }
+  return res;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double res = 0.0;
+  for (uint16_t i = i; i <= count; i++) {
+    double a = pown((-1), (i-1));
+    double b = pown(x, (2*i-1));
+    unit16_t c = fact(2*i-1);
+    res += a*(b/c);
+  }
+  return res;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double res = 0.0;
+  for (uint16_t i = i; i <= count; i++) {
+    double a = pown((-1), (i-1));
+    double b = pown(x, (2*i-2));
+    unit16_t c = fact(2*i-2);
+    res += a*(b/c);
+  }
+  return res;
 }
