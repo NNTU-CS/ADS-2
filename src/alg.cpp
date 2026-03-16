e8557a1f3376e9ed5a86f1a14abcb7cf24e04607
Артём <71376415+artemik2ww@users.noreply.github.com>
2026-03-16 22:36:24 +0300

    Update2 alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,6 +4,8 @@
 
 
 double pown(double value, uint16_t n) {
+	if (n == 0) return 1.0;
+
 	double ans = 1.0;
 	for (uint16_t i = 0; i < n; i++) {
 		ans *= value;
@@ -12,6 +14,8 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
+	if (n == 0 || n == 1) return 1;
+
 	uint16_t ans = 1;
 	for (uint16_t i = 2; i <= n; i++) {
 		ans *= i;
@@ -20,10 +24,13 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
+	if (n == 0) return 1.0;
 	return pown(x,n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
+	if (count == 0) return 0.0;
+
 	double ans = 0.0;
 	for (uint16_t i = 0; i < count; i++) {
 		ans += calcItem(x, i);
@@ -32,10 +39,13 @@ double expn(double x, uint16_t count) {
 }
 
 double sinn(double x, uint16_t count) {
+	if (count == 0) return 0.0;
+
 	double ans = 0.0;
 	for (uint16_t i = 0; i < count; i++) {
 		uint16_t power = 2 * i + 1;
-		double t = calcItem(x, power);
+		double t = calcItem(x, power) / fact(power);
+
 		if (i % 2 == 0) {
 			ans += t;
 		}
@@ -48,10 +58,11 @@ double sinn(double x, uint16_t count) {
 }
 
 double cosn(double x, uint16_t count) {
+	if (count == 0) return 0.0;
 	double ans = 0.0;
 	for (uint16_t i = 0; i < count; i++) {
 		uint16_t power = 2 * i;
-		double t = calcItem(x, power);
+		double t = pown(x, power) / fact(power);
 		if (i % 2 == 0) {
 			ans += t;
 		}
