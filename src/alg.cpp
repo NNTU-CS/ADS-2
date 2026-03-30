b92d4b92b8c0fa5b746829d60adb3d54e9c052a9
zudindaniil8-art <zudindaniil8@gmail.com>
2026-03-30 16:48:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,7 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
 #include "alg.h"
+
 double pown(double value, uint16_t n) {
     double result = 1.0;
     for (uint16_t i = 0; i < n; ++i) {
@@ -8,6 +9,7 @@ double pown(double value, uint16_t n) {
     }
     return result;
 }
+
 uint64_t fact(uint16_t n) {
     uint64_t result = 1;
     for (uint16_t i = 2; i <= n; ++i) {
@@ -15,22 +17,25 @@ uint64_t fact(uint16_t n) {
     }
     return result;
 }
+
 double calcItem(double x, uint16_t n) {
     if (n == 0) return 1.0;
     double numerator = pown(x, n);
     uint64_t denominator = fact(n);
     return numerator / static_cast<double>(denominator);
 }
+
 double expn(double x, uint16_t count) {
     double sum = 0.0;
-    for (uint16_t n = 0; n < count; ++n) {
+    for (uint16_t n = 0; n <= count; ++n) {
         sum += calcItem(x, n);
     }
     return sum;
 }
+
 double sinn(double x, uint16_t count) {
     double sum = 0.0;
-    for (uint16_t n = 0; n < count; ++n) {
+    for (uint16_t n = 0; n <= count; ++n) {
         uint16_t power = 2 * n + 1;
         double numerator = pown(x, power);
         uint64_t denominator = fact(power);
@@ -42,9 +47,10 @@ double sinn(double x, uint16_t count) {
     }
     return sum;
 }
+
 double cosn(double x, uint16_t count) {
     double sum = 0.0;
-    for (uint16_t n = 0; n < count; ++n) {
+    for (uint16_t n = 0; n <= count; ++n) {
         uint16_t power = 2 * n;
         double numerator = pown(x, power);
         uint64_t denominator = fact(power);
