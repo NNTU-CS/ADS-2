4dfae4ab6b8a52499fe998b5e7939bf7a6214f85
Vector-093 <motleo2007@gmail.com>
2026-05-10 16:33:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,48 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+	if (n == 0) return 1;
+	else {
+		double res = value;
+		for (; n > 1; n--) {
+			res *= value;
+		}
+		return res;
+	}
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+	uint64_t res = 1;
+	for (; n > 1; n--) {
+		res *= n;
+	}
+	return res;
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+	return (pown(x,n))/(fact(n));
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+	double res = 1;
+	for (uint16_t i = 1; i <= count; i++) {
+		res += calcItem(pown(x, i),i);
+	}
+	return res;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+	double res = 0;
+	for (uint16_t i = 1; i <= count ; i++) {
+		res += pown(-1,i-1)* calcItem(x,2*i-1);
+	}
+	return res;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+	double res = 0;
+	for (uint16_t i = 1; i <= count; i++) {
+		res += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
+	}
+	return res;
 }
