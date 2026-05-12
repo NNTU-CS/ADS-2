7db0576446def4e93413ff7e38f3aa6b635c4c86
belovarsenij2-glitch <belovarsenij2@gmail.com>
2026-05-13 00:11:58 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,7 +5,7 @@
 
 double pown(double value, uint16_t n) {
     double ans = 1.0;
-    for (uint16_t x = 0; x < n; x++) {
+    for (uint16_t i = 0; i < n; i++) {
         ans *= value;
     }
     return ans;
@@ -13,8 +13,8 @@ double pown(double value, uint16_t n) {
 
 uint64_t fact(uint16_t n) {
     uint64_t ans = 1;
-    for (uint16_t x = 2; x <= n; x++) {
-        ans *= x;
+    for (uint16_t i = 2; i <= n; i++) {
+        ans *= i;
     }
     return ans;
 }
@@ -35,13 +35,12 @@ double sinn(double x, uint16_t count) {
     double ans = 0.0;
     for (uint16_t n = 0; n < count; n++) {
         uint16_t power = 2 * n + 1;
-        double count = calcItem(x, power);
+        double t = calcItem(x, power);
 
         if (n % 2 == 0) {
-            ans += count;
-        }
-        else {
-            ans -= count;
+            ans += t;
+        } else {
+            ans -= t;
         }
     }
     return ans;
@@ -51,13 +50,12 @@ double cosn(double x, uint16_t count) {
     double ans = 0.0;
     for (uint16_t n = 0; n < count; n++) {
         uint16_t power = 2 * n;
-        double count = calcItem(x, power);
+        double t = calcItem(x, power);
 
         if (n % 2 == 0) {
-            ans += count;
-        }
-        else {
-            ans -= count;
+            ans += t;
+        } else {
+            ans -= t;
         }
     }
     return ans;
